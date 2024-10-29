a35a53c4ddbab3e3484f09813855d05773c8fa1b
vanessarcosta <vanessajreis38@gmail.com>
2024-10-29 14:34:20 +0000

    (Excs.alg): soma de tres valores

---
+++ Excs/ExcSoma/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExcSoma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa para somar três numeros inseridos pelo usuário
+
+            int numero1, numero2, numero3, resultado;
+
+            Console.Write("Insira o primeiro número:");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Insira o segundo número:");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Insira o terceiro número: ");
+            numero3 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = numero1 + numero2 + numero3;
+
+            Console.Clear();
+
+            Console.WriteLine($"{numero1} + {numero2} + {numero3} = {resultado}");
+
+
+
+        }
+    }
+}
