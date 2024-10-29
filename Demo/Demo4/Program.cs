f51000aacdd0f7db843a6acb13b05d51a5743b6c
vanessarcosta <vanessajreis38@gmail.com>
2024-10-29 15:12:57 +0000

    (Demo4.alg): calcular media

---
+++ Demo/Demo4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //Pedir dois números com casa decimais e calcular a sua média
+
+            double n1, n2, media;
+
+            Console.Write("Insira o primeiro valor: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira o segundo valor: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (n1 + n2) / 2;
+
+            Console.WriteLine($"A média dos valores introduzidos é de {media}");
+        }
+    }
+}
