9e6661e4270eb01f358528710a201213ea94f64c
vanessarcosta <vanessajreis38@gmail.com>
2024-10-29 14:55:40 +0000

    (Demo2.alg): processamento de dados

---
+++ Demo/Demo1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Isto é o projeto 1");
+        }
+    }
+}
