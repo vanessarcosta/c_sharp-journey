3850581e49a3a387d40107531585af432875b12d
vanessarcosta <vanessajreis38@gmail.com>
2024-10-29 18:47:52 +0000

    (Exc2.alg): converter moeda

---
+++ Exc/Exc4IMC/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exc4IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular o IMC
+
+            double peso, altura;
+
+            Console.Write("Digite o seu peso em kg: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o sua altura em metros: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"\nO seu IMC: {Math.Round(peso / (altura * altura),2)}");
+
+        }
+    }
+}
