b03e3fac958967dace7cef71cbe499e17d2a3fae
vanessarcosta <vanessajreis38@gmail.com>
2024-10-29 16:25:33 +0000

    (Exc1.alg): print mensagem

---
+++ Exc/Exc1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Exc1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome, apelido;
+
+            Console.Write("Insira o primeiro nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Insira o apelido: ");
+            apelido = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine($"Olá, {nome} {apelido}. Como vai? ");
+
+        }
+    }
+}
