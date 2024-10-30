281ee0dc6775b0c058147b67d591707db88f6c44
vanessarcosta <vanessajreis38@gmail.com>
2024-10-30 17:03:48 +0000

    (Exc9AzulejosParaParede.alg): calcular quantidade de azulejos

---
+++ Exc/Exc9.AzulejosParaParede/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exc9.AzulejosParaParede
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular a quantidade de azulejos para uma parede
+
+            double hp, ha, lp, la;
+
+            Console.Write("Digite em metros a altura da parede: ");
+            hp = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite em metros a largura da parede: ");
+            lp = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite em centímetros a altura do azulejo: ");
+            ha = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite em centímetros a largura do azulejo: ");
+            la = Convert.ToDouble(Console.ReadLine());
+
+            la = la / 100;
+            ha = ha / 100;
+            
+            Console.WriteLine($"\n Será necessário {Math.Ceiling((hp * lp) /(ha * la))} azulejos ");
+            
+        }
+    }
+}
