cdd9e8dfffdbedc9d5f36f496b7b2b0371524a8b
vanessarcosta <vanessajreis38@gmail.com>
2024-10-30 18:31:26 +0000

    (Exc10Imposto.alg): calcular imposto sobre area

---
+++ Exc/Exc10Imposto/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exc10Imposto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular imposto de um terreno sobre area total/construida
+
+            double areaTotal, areaConstrucao, imposto;
+
+            Console.Write("Digite em metros a área total do terreno: ");
+            areaTotal = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite em metros a área construída do terreno: ");
+            areaConstrucao = Convert.ToDouble(Console.ReadLine());
+
+            areaTotal = areaTotal - areaConstrucao;
+            imposto = areaTotal * 5.80 + areaConstrucao * 50.00; 
+                        
+            Console.WriteLine($"\n Valor total do imposto {Math.Round(imposto,2)} Euros ");
+
+        }
+    }
+}
