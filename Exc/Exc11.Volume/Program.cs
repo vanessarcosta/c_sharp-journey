60adf6e5c069a4fd592796ecf40626c3fc753933
vanessarcosta <vanessajreis38@gmail.com>
2024-10-30 18:44:46 +0000

    (Exc11Volume.alg): calcular o volume de uma lata

---
+++ Exc/Exc11.Volume/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exc11.Volume
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular o volume de uma lata de óleo
+
+            double v, r, a;             
+ ;
+            Console.Write("Informe a altura de uma lata de óleo: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe o raio de uma lata de óleo: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            v = 3.14159 * r * r * a;
+
+            Console.WriteLine($"\nValor do volume de uma lata de óleo {Math.Round(v,2)} ");
+        }
+    }
+}
