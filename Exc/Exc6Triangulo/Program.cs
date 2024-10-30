74fe41b97a8d650d88124e70d62a4f60d88d9826
vanessarcosta <vanessajreis38@gmail.com>
2024-10-30 12:58:59 +0000

    (Exc6Triangulo.alg): calcular area do triangulo

---
+++ Exc/Exc6Triangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exc6Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular a área  triângulo
+
+            double b, h;
+
+            Console.Write("Informe a base do triângulo: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe a altura do triângulo: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"\nA área do triângulo é = {Math.Round(b * h / 2, 2)} ");
+           
+        }
+    }
+}
