b1f84d0a5ac764a5c3bde09c6329c8bec6665195
vanessarcosta <vanessajreis38@gmail.com>
2024-10-30 20:12:27 +0000

    (Exc13TrocaDeVariaveld.alg): trocar variavel

---
+++ Exc/Exc13TrocadeVariavel/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exc13TrocadeVariavel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trocar de variável
+
+            int A, B, C;
+            
+            Console.Write("Digite o valor de A: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o valor de B: ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            C = A;
+            A = B;
+            B = C;
+
+            Console.WriteLine($"\nValor A = {A} e B = {B}");
+        }
+    }
+}
