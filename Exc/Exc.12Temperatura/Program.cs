dfe31d126401df08de382b75dd75229fb10b8c38
vanessarcosta <vanessajreis38@gmail.com>
2024-10-30 19:23:05 +0000

    (Exc12Temperatura.alg): converter temperatura

---
+++ Exc/Exc.12Temperatura/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exc._12Temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Converter temperatura de Fahrenheit para Celsius
+
+            double C,F;
+            
+            Console.Write("Digite a temperatura em Fahrenheit: ");
+            F = Convert.ToDouble(Console.ReadLine());
+
+            C = (F -32) *  (5.0 / 9.0);
+
+            Console.WriteLine($"\nTemperatura equivalente em Celsius = {Math.Round(C, 2)}");
+            
+        }
+    }
+}
