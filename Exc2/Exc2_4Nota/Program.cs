d4fe7c258385e55b9bcba9043b32819e377717e7
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 17:57:45 +0000

    (Exc2_4Nota.alg): verificar tabela nota

---
+++ Exc2/Exc2_4Nota/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exc2_4Nota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota;
+
+            Console.Write("Insira a nota: ");
+            nota = Convert.ToDouble(Console.ReadLine());
+
+
+            if (nota >= 0 && nota <= 20)
+            {
+                if (nota <= 5)
+                {
+                    Console.WriteLine("Fraco.");
+                }
+                else if (nota <= 9)
+                {
+                    Console.WriteLine("Insuficiente.");
+                }
+                else if (nota <= 14)
+                {
+                    Console.WriteLine("Satisfaz.");
+                }
+                else if (nota <= 18)
+                {
+                    Console.WriteLine("Bom.");
+                }
+                else if (nota <= 20)
+                {
+                    Console.WriteLine("Muito Bom.");
+                }           
+            }
+            else
+            {
+                Console.WriteLine("Não é uma nota válida.");
+            }
+        }
+    }
+}
