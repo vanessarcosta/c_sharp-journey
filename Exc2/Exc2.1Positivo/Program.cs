ef94182fc088e50e058e36225c7509eb3e6c2bd6
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 13:08:17 +0000

    configurando pasta solution: Exc2

---
+++ Exc2/Exc2.1Positivo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exc2._1Positivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
