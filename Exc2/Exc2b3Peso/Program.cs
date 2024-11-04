5a8605b4a124e091a12085d5ce2214a5b3f723e4
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 22:47:45 +0000

    (Exc2b3Peso.alg): calcular peso ideal

---
+++ Exc2/Exc2b3Peso/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exc2b3Peso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, peso;
+            char genero;
+
+            Console.WriteLine("Insira a altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira o sexo: ");
+            genero = Console.ReadKey().KeyChar;
+
+
+            if(genero == 'M'|| genero == 'm')
+            {
+                peso = 72.7 * altura -58;
+                Console.WriteLine($"\nPeso ideal: {Math.Round(peso,2)}");
+            }           
+            else if( genero == 'F' || genero == 'f')
+            {
+                peso = 62.1 * altura - 44.7;
+                Console.WriteLine($"\nPeso ideal {Math.Round(peso, 2)}");
+            }
+        }
+    }
+}
