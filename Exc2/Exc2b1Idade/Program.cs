647b586ab99c85e1591e4e62ba5b07eefa546722
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 22:05:44 +0000

    (Exc2b1Idade.alg): categorizar idade

---
+++ Exc2/Exc2b1Idade/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Exc2b1Idade
+{
+    internal class Program              
+    {
+        static void Main(string[] args)
+        {
+            int categoria;
+
+            Console.Write("Insira a idade do nadador: ");
+             categoria = Convert.ToInt32(Console.ReadLine());
+
+            if (categoria >= 18)
+            {
+                Console.WriteLine("Categoria adulto.");
+            }
+            else if (categoria >= 14)
+            {
+                Console.WriteLine("Categoria juvenil B");
+            }
+            else if (categoria >= 11)
+            {
+                Console.WriteLine("Categoria juvenil A");
+            }
+            else if (categoria >= 8)
+            {
+                Console.WriteLine("Categoria infantil B.");
+            }
+            else if (categoria >= 5)
+            {
+                Console.WriteLine("Categoria infantil A.");
+            }
+            else
+            {
+                Console.WriteLine("Sem categoria.");
+            }
+        }
+    }
+}
