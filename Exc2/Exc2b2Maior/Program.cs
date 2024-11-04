778ea332e0620ccb54d6d5ae3930ef6157339c41
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 22:15:41 +0000

    (Exc2b2Maior.alg): imprimr maior valor

---
+++ Exc2/Exc2b2Maior/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exc2b2Maior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("Insira o primeiro valor: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o segundo valor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o terceiro valor: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine($"Maior número = {num1}");
+            }
+            else if (num2 > num1 && num2 > num3)
+                Console.WriteLine($"Maior número = {num2}");
+            else
+            {
+                Console.WriteLine($"Maior número = {num3}");
+            }
+        }
+    }
+}
