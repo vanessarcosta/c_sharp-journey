a08773f573838c3cff06093719ee74831a4b4903
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 23:38:44 +0000

    (Exc2C3Maca.alg): calcular produto

---
+++ Exc2/Exc2C3Maca/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exc2C3Maca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco, quantidade;
+
+            Console.WriteLine("Insira a quantidade de maças: ");
+            quantidade = Convert.ToDouble(Console.ReadLine());
+           
+            if (quantidade < 12.0)
+            {
+                preco = quantidade * 0.3;
+                Console.WriteLine($"\nValor a pagar: {Math.Round(preco, 2)}");
+            }
+            else if (quantidade >= 12)
+            {
+                preco = quantidade * 0.25;
+                Console.WriteLine($"\nValor a pagar {Math.Round(preco, 2)}");
+            }
+        }
+    }
+}
