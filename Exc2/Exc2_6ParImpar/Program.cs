5d9730ac51eb8c570e6d02493f112c16ac5800a6
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 18:41:02 +0000

    (Exc2_6ParImpar.alg): verificar valor par

---
+++ Exc2/Exc2_6ParImpar/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exc2_6ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            Console.WriteLine("Insira um número: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Numero par.");
+            }
+            else
+            {
+                Console.WriteLine("Numero impar.");
+            }
+        }
+    }
+}
