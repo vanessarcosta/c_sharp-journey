b4858315e900a6603fb649174812187bbf291367
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 23:15:46 +0000

    (Exc2C1Votacao.alg): verificar se esta apto a votar

---
+++ Exc2/Exc2C1Votacao/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exc2C1Votacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int data;
+
+            Console.WriteLine("Digite o ano de nascimento:");
+            data = Convert.ToInt32(Console.ReadLine());
+
+            if (data > 2006)
+            {
+                Console.WriteLine("Não está apto a votar");
+            }
+            else
+            { 
+                Console.WriteLine("Está apto a votar");
+            }
+        }
+    }
+}
