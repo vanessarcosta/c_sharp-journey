6ca6c7ba3da9fff8e7c00e6786abfe70004a5e0f
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 16:34:42 +0000

    (Exc2_3Maior.alg): verificar qual o numero maior

---
+++ Exc2/Exc2_3Maior/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exc2_3Maior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+            Console.Write("Insira o primeiro número: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insira o segundo número: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine($"O {numero1} é maior!");
+            }
+            else
+            {
+                Console.WriteLine($"O {numero2} é maior!");
+            }
+        }
+    }
+}
---
+++ Exc2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exc2._1Positivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Insira um número inteiro: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero >= 0)
+            {
+                Console.WriteLine($"O {numero} é positivo!");
+            }
+            else
+            {
+                Console.WriteLine($"O {numero} é negativo!");
+            }
+
+        }
+    }
+}
