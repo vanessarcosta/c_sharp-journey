1a1a50be8ee28f65908695f898568b74d84740ef
vanessarcosta <vanessajreis38@gmail.com>
2024-11-04 23:28:05 +0000

    (Exc2C2Senha.alg): verificar senha

---
+++ Exc2/Exc2C2Senha/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exc2C2Senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+            Console.WriteLine("Digite a senha: ");
+            senha = Convert.ToInt32(Console.ReadLine());
+
+            while (senha != 1234) 
+            {
+                Console.WriteLine("Senha Inválida! Tente novamente.");                
+                Console.WriteLine("Digite a senha: ");                
+                senha = Convert.ToInt32(Console.ReadLine());         
+            }
+            Console.WriteLine("Acesso permitido!");
+        }
+    }
+}
