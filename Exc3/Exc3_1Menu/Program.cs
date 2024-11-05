72ed0ff145d33dcac1d4592c021205b18aee0d4f
vanessarcosta <vanessajreis38@gmail.com>
2024-11-05 15:50:41 +0000

    config(Exc3.alg): config pasta solution

---
+++ Exc3/Exc3_1Menu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exc3_1Menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
