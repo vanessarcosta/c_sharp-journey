9162df125cedb199686b35192e419dec6df8e1b9
vanessarcosta <vanessajreis38@gmail.com>
2024-11-05 12:43:49 +0000

    (Exc2C8ValidarData.alg): validar data

---
+++ Exc2/Exc2C8ValidarData/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exc2C8ValidarData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, ano;
+
+            Console.Write("Digite o dia de nascimento: ");
+            dia = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o mês de nascimento: ");
+            mes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o ano de nascimento: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+
+            if (dia <= 0 || dia > 31)
+            {
+                Console.WriteLine($"Valor {dia} informado não representa uma data válida.Digite valor entre 1 e 31");
+            }
+            else if (mes <= 0 || mes > 12)
+            {
+                Console.WriteLine($"Valor {mes} informado não representa uma data válida. Digite valor entre 1 e 12.");
+            }
+            else if (ano > 2024)
+            {
+                Console.WriteLine($"Valor {ano} informado não representa uma data válida.Digite valor menor 2024");
+            }
+        }
+    }
+}
