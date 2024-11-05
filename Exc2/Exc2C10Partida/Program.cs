2ee10f7931f6be5df860908cf2f6efc8f3fbca39
vanessarcosta <vanessajreis38@gmail.com>
2024-11-05 14:10:56 +0000

    (Exc2C910Partida.alg): verificar resultado

---
+++ Exc2/Exc2C10Partida/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exc2C10Partida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int golsAfavor, golsContra;
+
+            Console.Write("Digite os gols marcados pela equipe: ");
+            golsAfavor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Dgite os gols sofridos pela equipe: ");
+            golsContra = Convert.ToInt32(Console.ReadLine());
+
+
+            if (golsAfavor == golsContra)
+            {
+                Console.WriteLine("A equipe empatou a partida!");
+            }
+            else if (golsAfavor > golsContra)
+            {
+                Console.WriteLine("A equipe ganhou a partida! ");
+            }
+            else
+            {
+                Console.WriteLine("A equipe perdeu a partida.");
+            }
+        }
+    }
+}
