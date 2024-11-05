9f92fbcf9e8e6a4ccc6570f4cff2360652103613
vanessarcosta <vanessajreis38@gmail.com>
2024-11-05 15:20:51 +0000

    (Exc2C911Reservatorio.alg): calcular autonomia reservatório

---
+++ Exc2/Exc2C11Reservatorio/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exc2C11Reservatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura, largura, comprimento, consumoMedio;
+            double autonomia;    
+            
+            Console.Write("Digite a altura do reservatório: ");
+            altura = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite a largura do reservatório: ");
+            largura = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite a comprimento do reservatório: ");
+            comprimento = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o consumo médio diário de água: ");
+            consumoMedio = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nA capacidade total do reservátorio é de {Math.Pow(comprimento, 3) * 1000.0}.");
+            autonomia = Math.Pow(comprimento, 3) * 1000.0 / consumoMedio;
+            Console.WriteLine($"\nA autonomia do reservátorio é de {autonomia} dias.");
+
+            if (autonomia >= 7.0)
+            {
+                Console.WriteLine("Consumo reduzido.");
+            }
+            else if (autonomia >= 2)
+            {
+                Console.WriteLine("Consumo moderado.");
+            }
+            else
+            {
+                Console.WriteLine("Consumo elevado.");
+            }
+        }
+    }
+}
