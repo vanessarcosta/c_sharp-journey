f0698c6b5bdecc1818e5a2b42e61fee3294090d5
vanessarcosta <vanessajreis38@gmail.com>
2024-11-05 12:13:56 +0000

    (Exc2C7Data.alg): validar data

---
+++ Exc2/Exc2C7Data/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exc2C7Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+           
+            Console.Write("Digite a idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+                        
+            if (idade >= 18 && idade <=67)
+            {
+                Console.WriteLine("Apto a fazer a doação de sangue.");
+            }
+            else
+            {                
+                Console.WriteLine("Apto a fazer a doação de sangue.");
+            }
+        }
+    }
+}
