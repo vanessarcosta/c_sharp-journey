c0f8e32099fcaef584e6ae21b125b1381c21d62d
vanessarcosta <vanessajreis38@gmail.com>
2024-11-05 12:52:51 +0000

    (Exc2C9Divisor5.alg): verificar divisor 5

---
+++ Exc2/Exc2C9Divisor5/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
+
+namespace Exc2C9Divisor5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());         
+
+            if (numero % 5 == 0)
+            {
+                Console.WriteLine($"O número {numero} é divísivel por 5");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} não é divísivel por 5");
+            }            
+        }
+    }
+}
