80b382965e960fe70301f15efbfeda58146eb9eb
vanessarcosta <vanessajreis38@gmail.com>
2024-11-07 23:56:46 +0000

    feat (Exc4_7Combinacoes.alg): imprimir combinações

---
+++ Exc4/Exc4_8Combinacoes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exc4_8Combinacoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NP, NC,a = 1, b = 1;
+
+            Console.WriteLine("Digite um número de pontos");
+            int.TryParse(Console.ReadLine(), out NP);
+            Console.WriteLine("Digite um número de casas");
+            int.TryParse(Console.ReadLine(), out NC);
+
+            while (a <= NP)
+            {               
+                while (b <= NC)
+                {                   
+                    Console.WriteLine($"{a} - {b}");
+                    b++;
+                }
+                b = 1;
+                a++;
+            }
+
+        }
+    }
+}
