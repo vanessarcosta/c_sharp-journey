5bfb1f029e340c8fc9795535286e5a5f02f4edec
vanessarcosta <vanessajreis38@gmail.com>
2024-11-07 15:23:43 +0000

    feat (Exc4_2ListaNumSemDivisor5.alg): imprimir listagem numeros indivisiveis por 5

---
+++ Exc4/Exc4_2ListaNumSemDivisor5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exc4_2ListaNumSemDivisor5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine(numero);
+            do
+            {                
+                numero++;
+
+                if (numero % 5 !=0.0)
+                {                  
+                    Console.WriteLine(numero);
+                }                
+            }
+            while (numero < 20);
+
+        }
+    }
+}
