015fb2085efbc36e52eb62626df5d13239315c47
vanessarcosta <vanessajreis38@gmail.com>
2024-11-07 21:53:06 +0000

    feat (Exc4_5Tabuada.alg): imprimir  tabuada

---
+++ Exc4/Exc4_5Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exc4_5Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out x);
+
+            Console.Clear();
+            while (y <= 9)
+            {
+                y++;
+                Console.WriteLine($"{x} * {y} = {x * y}");
+            }
+        }
+    }
+}
