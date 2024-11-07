89721b71956cd0f07131835fc78e16ca0cad9910
vanessarcosta <vanessajreis38@gmail.com>
2024-11-07 22:41:08 +0000

    feat (Exc4_6SomPares.alg): somátorio de pares do intervalo

---
+++ Exc4/Exc4_7SomPares/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exc4_7SomPares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0, numero2 = 0, aux = 0;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out numero1);
+            Console.WriteLine("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out numero2);
+
+            do
+            {                
+                if (numero1 % 2.0 == 0)
+                {                    
+                    aux += numero1;
+                }
+                numero1++;
+            }
+            while (numero1 < numero2 - 1);
+            Console.WriteLine($"Somatório do intervalo = {aux}");
+        }
+    }
+}
