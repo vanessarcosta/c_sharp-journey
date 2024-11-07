fcf51155d8ac23460b87b8a7f915a524df0555b3
vanessarcosta <vanessajreis38@gmail.com>
2024-11-07 18:50:31 +0000

    feat (Exemplo2.alg): Tryparse

---
+++ Ciclos/Exemplo2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            bool teste;
+
+            Console.WriteLine("Insira um número");
+            //teste = int.TryParse(Console.ReadLine(), out numero);
+
+            if (int.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.WriteLine("Conseguiu converter");
+            }
+            else
+            {
+                Console.WriteLine("Não consegiu converter");
+            }
+
+            Console.WriteLine(numero);
+           // Console.WriteLine(teste);
+
+            //TryParse
+        }
+    }
+}
