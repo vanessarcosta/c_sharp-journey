f606872d4514b6520f5ddc7ecae84f6cc6d275c8
vanessarcosta <vanessajreis38@gmail.com>
2024-11-07 22:14:20 +0000

    feat (Exc4_6Somatorio.alg): somátorio intervalo

---
+++ Exc4/Exc4_6Somatorio/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exc4_6Somatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0, numero2 = 0, aux = 0;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out numero1);
+            Console.WriteLine("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out numero2);
+
+            do
+            {
+                numero1++;
+                aux += numero1;                
+            }
+            while (numero1 < numero2 - 1);
+            Console.WriteLine($"Somatório do intervalo = {aux}");
+
+        }
+    }
+}
