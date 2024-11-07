1f396e752da2e68634087b83225495b5a0040a1e
vanessarcosta <vanessajreis38@gmail.com>
2024-11-07 13:18:04 +0000

    feat (Exc3_1PorExtenso.alg): imprimir numeros

--- Exc3/Exc3_1Menu/Program.cs
+++ Exc3/Exc3_1Menu/Program.cs
@@ -8,6 +8,7 @@ namespace Exc3_1Menu
         {
             int codigo, quantidade;
             double precoTotal=0;
+
             Console.WriteLine("Especificação                Código           Preço");
             Console.WriteLine("Cachorro Quente                100             2.50");
             Console.WriteLine("Hambúrguer Simples             101             3.50");
@@ -16,33 +17,36 @@ namespace Exc3_1Menu
             Console.WriteLine("Bifana com queijo e bacon      104             3.55");
             Console.WriteLine("Refrigerante                   105             1.75");
 
-            Console.Write("\nInsira o código: ");
-            codigo = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Insira o quantidade: ");
-            quantidade = Convert.ToInt32(Console.ReadLine());
-            
-            
-            switch (codigo)
+            do
             {
-                case 100:
-                    precoTotal = quantidade * 2.50;
-                break;
-                case 101:
-                    precoTotal = quantidade * 3.50;
-                break;
-                case 102:
-                    precoTotal = quantidade * 4.50;
-                    break;
-                case 103:
-                    precoTotal = quantidade * 2.70;
-                    break;
-                case 104:
-                    precoTotal = quantidade * 3.55;
-                    break;
-                case 105:
-                    precoTotal = quantidade * 1.75;
-                    break;
+                Console.Write("\nInsira o código: ");
+                codigo = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Insira o quantidade: ");
+                quantidade = Convert.ToInt32(Console.ReadLine());
+
+                switch (codigo)
+                {
+                    case 100:
+                        precoTotal += quantidade * 2.50;
+                        continue;
+                    case 101:
+                        precoTotal += quantidade * 3.50;
+                        continue;
+                    case 102:
+                        precoTotal += quantidade * 4.50;
+                        continue;
+                    case 103:
+                        precoTotal += quantidade * 2.70;
+                        continue;
+                    case 104:
+                        precoTotal += quantidade * 3.55;
+                        continue;
+                    case 105:
+                        precoTotal += quantidade * 1.75;
+                        continue;                         
+                }
             }
+            while (codigo != 0);        
 
             Console.WriteLine($"O valor da conta é {precoTotal} euros");
         }
---
+++ Exc3/Exc3_1PorExtenso/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Exc3_1PorExtenso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine("Digite um número!");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            string numeroExtenso = numero switch
+            {
+                0 => "zero",
+                1 => "um",
+                2 => "dois",
+                3 => "três",
+                4 => "quatro",
+                5 => "cinco",
+                6 => "seis",
+                7 => "sete",
+                8 => "oito",
+                9 => "nove",
+                10 => "dez",                
+            };
+            Console.WriteLine($"Numero por extenso: {numeroExtenso}");
+
+        }
+    }
+}
