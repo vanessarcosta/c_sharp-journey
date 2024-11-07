4519dfcac9215d459ec573177eb8b8c25c9919f7
vanessarcosta <vanessajreis38@gmail.com>
2024-11-07 21:19:21 +0000

    feat (Exc4_3ListaNumIntervalo.alg): imprimir intervalo de leitura

---
+++ Exc4/Exc4_3ListaNumIntervalo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exc4_3ListaNumIntervalo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0, numero2 = 0 , aux = 0;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out numero1);
+            Console.WriteLine("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out numero2);
+
+            if (numero1 > numero2) 
+            {
+                aux = numero1 ;
+                numero1 = numero2;
+                numero2 = aux;
+
+            }
+
+            do 
+            {                
+                numero1++;
+                Console.WriteLine(numero1);
+            }
+            while (numero1 < numero2 -1);
+
+        }
+    }
+}
