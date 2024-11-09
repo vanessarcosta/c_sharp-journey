894ec4ad58ea3802f033ee8aa4e5a0f09f8e3b2e
vanessarcosta <vanessajreis38@gmail.com>
2024-11-09 15:47:57 +0000

    feat (Exemplo4): imprimir intervalo

---
+++ Ciclos/Exemplo4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exemplo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //programa para pedir ao utilizador um número
+            //e mostrar uma listagem dos 20 números seguintes
+
+            int numero, limiteSuperior;
+
+            do
+            {
+                Console.Write("Insira um número inteiro: ");
+
+            }
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            limiteSuperior = numero + 20;
+
+            while (numero <= limiteSuperior)
+            {
+                Console.WriteLine(numero);
+                numero++;
+            }
+        }
+    }
+}
