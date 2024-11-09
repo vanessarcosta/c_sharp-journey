d4faf4871e7164d2e829e2602662a671253831ac
vanessarcosta <vanessajreis38@gmail.com>
2024-11-09 21:51:52 +0000

    feat (Exc4_11Pesquisa): implementado verificação de idade

--- Exc4/Exc4_11Pesquisa/Program.cs
+++ Exc4/Exc4_11Pesquisa/Program.cs
@@ -4,36 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        int idade;
+        int idade, maiorIdade, menorIdade;
         double salario;
         char sexo, op;
+        
+        
 
         do
         {
 
-        do
-        {
-            Console.WriteLine("Informe a idade: ");
-        }
-        while (!int.TryParse(Console.ReadLine(), out idade));
+            do
+            {
+                Console.WriteLine("Informe a idade: ");
+            }
+             while (!int.TryParse(Console.ReadLine(), out idade));
 
-        do
-        {
-            Console.WriteLine("Informe o sexo (M/F)");
-        }
-        while (!char.TryParse(Console.ReadLine(), out sexo));
+            do
+            {
+                Console.WriteLine("Informe o sexo (M/F)");
+            }
+            while (!char.TryParse(Console.ReadLine(), out sexo));
 
-        do
-        {
-            Console.WriteLine("Informe o salário: ");
-        }
-        while (!double.TryParse(Console.ReadLine(), out salario));
+            do
+            {
+                Console.WriteLine("Informe o salário: ");
+            }
+            while (!double.TryParse(Console.ReadLine(), out salario));
 
+           
+            try
+            {
+
+                if (maiorIdade < idade)
+                {
+                    maiorIdade = idade;
+                }
+
+                if (menorIdade > idade)
+                {
+                    menorIdade = idade;
+                }
+            } 
+            catch
+            {
+                (Console.WriteLine("Erro.");
+            }
             do
             {
                 Console.WriteLine("Continuar a inserir dados (S/N)");
             }
             while (!char.TryParse(Console.ReadLine(), out op));
+
         }
         while (op == 's' || op == 'S');
 
