28e8ee13e9e8005c17868dafa545d05da8882b55
vanessarcosta <vanessajreis38@gmail.com>
2024-11-09 22:43:06 +0000

    feat (Exc4_11Pesquisa): implementado print

--- Exc4/Exc4_11Pesquisa/Program.cs
+++ Exc4/Exc4_11Pesquisa/Program.cs
@@ -4,15 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        int idade, maiorIdade, menorIdade;
-        double salario;
+        int idade, totalHabitantes = 0, qtdMulher = 0;
+        double salario, totalSalario = 0;
         char sexo, op;
-        
-        
+        int maiorIdade = int.MinValue;
+        int menorIdade = int.MaxValue;
 
         do
         {
-
             do
             {
                 Console.WriteLine("Informe a idade: ");
@@ -29,26 +28,26 @@ class Program
             {
                 Console.WriteLine("Informe o salário: ");
             }
-            while (!double.TryParse(Console.ReadLine(), out salario));
+            while (!double.TryParse(Console.ReadLine(), out salario));    
 
-           
-            try
+            if (maiorIdade < idade)
             {
-
-                if (maiorIdade < idade)
-                {
-                    maiorIdade = idade;
-                }
-
-                if (menorIdade > idade)
-                {
-                    menorIdade = idade;
-                }
-            } 
-            catch
-            {
-                (Console.WriteLine("Erro.");
+                maiorIdade = idade;
             }
+
+            if (menorIdade > idade)
+            {
+                menorIdade = idade;
+            }
+
+            totalHabitantes++;
+            totalSalario += salario;
+
+            if (sexo == 'F' || sexo == 'f' && salario <= 500.0)
+            {
+                qtdMulher++;
+            }        
+
             do
             {
                 Console.WriteLine("Continuar a inserir dados (S/N)");
@@ -57,6 +56,10 @@ class Program
 
         }
         while (op == 's' || op == 'S');
+
+        Console.WriteLine($"A média de salário do grupo: {totalSalario / totalHabitantes}.");
+        Console.WriteLine($"A maior idade é {maiorIdade}, e a menor idade é {menorIdade}.");
+        Console.WriteLine($"Quantidade de mulheres com salário até 500 euros: {qtdMulher}.");
 
     }
 }
