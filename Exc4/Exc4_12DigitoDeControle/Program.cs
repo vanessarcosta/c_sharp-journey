e01ce90e1472d726446b2e2c81884b652fc1a79b
vanessarcosta <vanessajreis38@gmail.com>
2024-11-09 23:38:35 +0000

    feat (Exc4_12DigitoDeControlo): implementado menu e conversão para string

---
+++ Exc4/Exc4_12DigitoDeControle/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exc4_12DigitoDeControle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            do
+            {
+                Console.Write("Digite um número");
+            }
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            string numeroStr = numero.ToString();
+
+        }
+    }
+}
