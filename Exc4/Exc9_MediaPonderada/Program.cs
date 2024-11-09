196ab47f2ccae8d7dbcea675533730f5ae0b3a81
vanessarcosta <vanessajreis38@gmail.com>
2024-11-09 18:23:32 +0000

    code review (Exc9_MediaPonderada): refinamento do codigo

--- Exc4/Exc9_MediaPonderada/Program.cs
+++ Exc4/Exc9_MediaPonderada/Program.cs
@@ -4,40 +4,58 @@
     {
         static void Main(string[] args)
         {
-            int codigo = 0;
-            double nota1, nota2, nota3, media = 0.0;
+            int codigo;
+            double nota1, nota2, nota3, media;   
 
-            Console.Write("Digite o código do aluno: ");
-            int.TryParse(Console.ReadLine(), out codigo);
-            Console.WriteLine("Digite as três notas: ");
-            double.TryParse(Console.ReadLine(), out nota1);
-            Double.TryParse(Console.ReadLine(), out nota2);
-            double.TryParse(Console.ReadLine(), out nota3);
+            do
+            {
+                Console.Write("Digite o código do aluno: ");
+            }
 
+            while (!int.TryParse(Console.ReadLine(), out codigo));
+
+            do
+            {
+                Console.WriteLine("Digite a primeira nota: ");
+            }
+            while (!double.TryParse(Console.ReadLine(), out nota1));
+
+            do
+            {
+                Console.WriteLine("Digite a segunda nota: ");
+            }
+            while (!Double.TryParse(Console.ReadLine(), out nota2));
+
+            do
+            {
+                Console.WriteLine("Digite a terceira nota: ");
+            }
+            while (!double.TryParse(Console.ReadLine(), out nota3));
+            
+            
             if (nota1 > nota2 && nota1 > nota3)
             { 
-                media = (nota1 * 4.0 + nota2 * 3.0 + nota3 * 3.0) / 10;
+                media = Math.Round((nota1 * 4.0 + nota2 * 3.0 + nota3 * 3.0) / 10);
             }
             else if (nota2 > nota3)
             {
-                media = (nota1 * 3.0 + nota2 * 4.0 + nota3 * 3.0) / 10;
+                media =  Math.Round((nota1 * 3.0 + nota2 * 4.0 + nota3 * 3.0) / 10);
             }
             else 
             {
-                media = (nota1 * 3.0 + nota2 * 3.0 + nota3 * 4.0) / 10;
+                media = Math.Round((nota1 * 3.0 + nota2 * 3.0 + nota3 * 4.0) / 10);
             }
-             
-            if (media >= 5)
+
+            Console.WriteLine($"O aluno com código n.{codigo} obteve as seguintes notas:");
+            Console.WriteLine($"{nota1}; {nota2} e {nota3}.");
+
+            if (media >= 5.0)
             {
-                Console.WriteLine($"O aluno com código n.{codigo} obteve as seguintes notas:");
-                Console.WriteLine($"{Math.Round(nota1,2)}; {Math.Round(nota2,2)} e {Math.Round(nota3,2)}.");
-                Console.WriteLine($"Obteve média = {Math.Round(media,2)}. Sendo APROVADO!");
+                Console.WriteLine($"Obteve média = {media}. Sendo APROVADO!");
             }
             else
             {
-                Console.WriteLine($"O aluno com código n.{codigo} obteve as seguintes notas:");
-                Console.WriteLine($"{Math.Round(nota1,2)}; {Math.Round(nota2,2)} e {Math.Round(nota3,2)}.");
-                Console.WriteLine($"e obteve média = { Math.Round(media, 2)}. Sendo REPROVADO!");
+                Console.WriteLine($"e obteve média = {media}. Sendo REPROVADO!");
             }
 
         }
