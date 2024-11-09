6a7abfbd766bf9308c7eb859c137cfb677733032
vanessarcosta <vanessajreis38@gmail.com>
2024-11-09 18:44:51 +0000

    feat (Exc4_11Pesquisa): implementando menu

---
+++ Exc4/Exc4_11Pesquisa/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exc4_11Pesquisa;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade;
+        double salario;
+        char sexo;
+
+        do
+        {
+            Console.WriteLine("Informe a idade: ");
+        }
+        while (!int.TryParse(Console.ReadLine(), out idade));
+
+        do
+        {
+            Console.WriteLine("Informe o sexo (M/F)");
+        }
+        while (!char.TryParse(Console.ReadLine(), out sexo));
+
+        do
+        {
+            Console.WriteLine("Informe o salário: ");
+        }
+        while (!double.TryParse(Console.ReadLine(), out salario));
+
+    }
+}
