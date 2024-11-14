af86afc05c459e00939852dc1a4f78c38d26bbff
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 23:55:41 +0000

    feat (Exc4For_1ListaNumeros): imprimir lista de numeros

--- Exc4For/Exc4For_1ListaNumeros/Program.cs
+++ Exc4For/Exc4For_1ListaNumeros/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Exc4For_1ListaNumeros
+﻿using System;
+
+namespace Exc4For_1ListaNumeros
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numero = 0;
+
+            for (; numero <= 20; numero++)
+            {
+                Console.WriteLine(numero);
+            }     
         }
     }
 }
