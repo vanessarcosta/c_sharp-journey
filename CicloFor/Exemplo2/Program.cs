aea185fe6a7a0f363c46a1e842cbb15a476ce448
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 14:00:32 +0000

    feat (Exemplo2): imprimir nome completo usando for

---
+++ CicloFor/Exemplo2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, apelido;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                //corpo do ciclo
+                Console.Write($"Insira o nome {i}: ");
+                nome = Console.ReadLine();
+
+                Console.Write($"Insira o apelido {i}: ");
+                apelido = Console.ReadLine();
+
+                Console.WriteLine($"Olá, {nome} {apelido}");
+
+            }
+        }
+    }
+}
