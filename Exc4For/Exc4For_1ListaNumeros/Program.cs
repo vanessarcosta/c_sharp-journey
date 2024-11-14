81822625ab7f3ae1481502a74f5d596b4f92732b
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 23:50:55 +0000

    config(Exc4For): config pasta solution

---
+++ Exc4For/Exc4For_1ListaNumeros/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exc4For_1ListaNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
