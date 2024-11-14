45a2549a0c3f20e631848d50896bb3a2d094c86f
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 15:40:02 +0000

    feat (Exc5_2Factorial): calcular fatorial *

---
+++ Exc5/Exc5_2Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exc5_2Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, fatorial = 1;
+
+            do
+            {
+                Console.Write("Digite a altura do retângulo: ");
+            }
+
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            for (int i = 1; i <= numero; i++)
+            {
+                fatorial = fatorial * i;
+            }
+
+            Console.WriteLine($"Fatorial de {numero} = {fatorial}");
+        }
+    }
+}
