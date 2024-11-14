8d7b8a19a9bd9b0f61c101e7c5f99608c0939cfa
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 21:48:50 +0000

    feat (Exc5_4CapitalAcumulado): calcular capital

---
+++ Exc5/EXC5_4CapitalAcumulado/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EXC5_4CapitalAcumulado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double capital = 1500.0, taxaAnual = 0.02, capitalAcumulado = 0;
+
+            for (int anos = 1; anos <= 3; anos++)
+            {
+                capitalAcumulado = capital * Math.Pow((1 + taxaAnual), anos);
+                taxaAnual += 0.005;
+                Console.WriteLine($"O capital total é de {Math.Round(capitalAcumulado, 2)} euros em {anos} ano(s).");
+            }            
+        }
+    }
+}
