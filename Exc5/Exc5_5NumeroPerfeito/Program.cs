1141d3f10819c0be77921a106faa292e5976e98b
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 22:16:45 +0000

    feat (Exc5_5NumeroPerfeito): verificar se o numero perfeito

---
+++ Exc5/Exc5_5NumeroPerfeito/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+namespace Exc5_5NumeroPerfeito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, divisores = 0;
+
+            do
+            {
+                Console.Write("Insira um valor: ");
+            }
+
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            for (int i = 1; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    divisores += i;
+                }
+            }
+
+            if (divisores == numero)
+            {
+                Console.WriteLine($"O número {numero} é perfeito.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero}  não é perfeito.");
+            }
+        }
+    }   
+}
