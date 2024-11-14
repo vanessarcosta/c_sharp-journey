8c1ac1c401ecde31e18f2fb3d9ddb6bcb16cadd2
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 14:47:32 +0000

    config(Exc5): config pasta solution

---
+++ Exc5/Exc5_1Retangulo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exc5_1Retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
