8371afe344ed3c76bcadec30f5e271502904d239
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 14:41:31 +0000

    feat (Exemplo4): uso encadeado for

---
+++ CicloFor/Exemplo4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exemplo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    for (int k = 1; k <= 3; k++)
+                    {
+                        Console.WriteLine($"{i} {j} {k}");
+                    }
+                }
+                
+            }
+        }
+    }
+}
