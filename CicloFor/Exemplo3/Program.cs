9c1bf5de44dfe8041949b47a0548200f29d14263
vanessarcosta <vanessajreis38@gmail.com>
2024-11-14 14:25:10 +0000

    feat (Exemplo3): somar intervalo dado

---
+++ CicloFor/Exemplo3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exemplo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //somatório de um intervalo dado
+            //limite inferior -> 10
+            //limite superior -> 15
+            //10+11+12+13+14+15 = 75
+
+            int limiteInferior, limiteSuperior, soma = 0;
+
+            Console.Write("Limite inferior: ");
+            limiteInferior = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Limite superior: ");
+            limiteSuperior = Convert.ToInt32(Console.ReadLine());
+
+          //  for (int i = limiteInferior; i <= limiteSuperior; i++)
+         // {
+        //     soma += i;                
+        //    }
+
+            for (; limiteInferior <= limiteSuperior; limiteInferior++)
+            {
+                soma += limiteInferior;
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
