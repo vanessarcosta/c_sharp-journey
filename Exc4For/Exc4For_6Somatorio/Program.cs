81c0096bf7c6c5207bcec0b17dac0234b9ef726b
vanessarcosta <vanessajreis38@gmail.com>
2024-11-15 00:29:36 +0000

    feat (Exc4For_6Somatorio): calcular somatorio intervalo dado

---
+++ Exc4For/Exc4For_6Somatorio/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exc4For_6Somatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0, numero2 = 0, aux = 0;
+
+            Console.Write("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out numero1);
+            Console.Write("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out numero2);
+
+            numero1 += 1;
+            for (; numero1 < numero2; numero1++)
+            {
+                aux += numero1;
+            }
+
+            Console.WriteLine($"Somatório do intervalo = {aux}");
+        }
+    }
+}
