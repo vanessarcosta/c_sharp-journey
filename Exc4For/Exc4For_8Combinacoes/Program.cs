410ce4f86b476569cebc2db862ce8902d048c9bc
vanessarcosta <vanessajreis38@gmail.com>
2024-11-15 00:44:05 +0000

    feat (Exc4For_8Combinacoes): imprimir combinaçoes

---
+++ Exc4For/Exc4For_8Combinacoes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exc4For_8Combinacoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NP, NC, a = 1, b = 1;
+
+            Console.Write("Digite um número de pontos: ");
+            int.TryParse(Console.ReadLine(), out NP);
+            Console.Write("Digite um número de casas: ");
+            int.TryParse(Console.ReadLine(), out NC);
+
+
+            for (; a <= NP; a++)
+            {
+                for (global::System.Int32 j = 0; b <= NC; b++)
+                {
+                    Console.Write($" {a}-{b} \t");
+                }
+                b = 1;
+            }
+        }
+    }
+}
