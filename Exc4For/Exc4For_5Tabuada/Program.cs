bcde9476e41252946f15fc9f14fa350abcbcb0fa
vanessarcosta <vanessajreis38@gmail.com>
2024-11-15 00:17:27 +0000

    feat (Exc4For_5Tabuada): calcular a tabuada

---
+++ Exc4For/Exc4For_5Tabuada/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exc4For_5Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 1;
+
+            Console.WriteLine("Digite um número: ");
+            int.TryParse(Console.ReadLine(), out x);
+
+            Console.Clear();
+            for (; y <= 10; y++)
+            {
+                Console.WriteLine($"{x} * {y} = {x * y}");
+            }
+        }
+    }
+}
