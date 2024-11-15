709ba2eb2145edf6ea7d8ad2ff6b88f3173f6e76
vanessarcosta <vanessajreis38@gmail.com>
2024-11-15 00:34:13 +0000

    feat (Exc4For_7SomPares: calcular somatorio de pares intervalo dado

---
+++ Exc4For/Exc4For_7SomPares/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exc4For_7SomPares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0, numero2 = 0, aux = 0;
+
+            Console.Write("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out numero1);
+            Console.Write("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out numero2);
+
+            numero1 += 1;
+            for (; numero1 < numero2; numero1++)
+            {
+                if (numero1 % 2.0 == 0)
+                {
+                    aux += numero1;
+                }
+            }
+
+            Console.WriteLine($"Somatório do intervalo = {aux}");
+        }
+    }
+}
