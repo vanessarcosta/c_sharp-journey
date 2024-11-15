a04db617164aba141a5dc5cb984a54dc7943ea21
vanessarcosta <vanessajreis38@gmail.com>
2024-11-15 00:06:19 +0000

    feat (Exc4For_3ListaNumIntervalo): imprimir lista de intervalo dado

---
+++ Exc4For/ExcFor_3ListaNumIntervalo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ExcFor_3ListaNumIntervalo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0, numero2 = 0, aux = 0;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out numero1);
+            Console.WriteLine("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out numero2);
+
+            if (numero1 > numero2)
+            {
+                aux = numero1;
+                numero1 = numero2;
+                numero2 = aux;
+
+            }
+
+            for (; numero1 < numero2 - 1; numero1++)
+            {
+                Console.WriteLine(numero1);
+            }
+        }
+    }
+}
