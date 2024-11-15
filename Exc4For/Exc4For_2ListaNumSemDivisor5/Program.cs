dc0e67459ce1e1860e49b886d9087e74096d8a41
vanessarcosta <vanessajreis38@gmail.com>
2024-11-15 00:00:20 +0000

    feat (Exc4For_2ListaNumeroSemDivisor5): imprimir lista de numeros exceto divisores 5

---
+++ Exc4For/Exc4For_2ListaNumSemDivisor5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exc4For_2ListaNumSemDivisor5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine(numero);
+
+            for (; numero < 20; numero++)
+            {
+                if (numero % 5 != 0.0)
+                {
+                    Console.WriteLine(numero);
+                }            }
+         
+        }
+    }
+}
