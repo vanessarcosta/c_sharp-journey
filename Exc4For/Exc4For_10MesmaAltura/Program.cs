2610df77640f8e2b63cb2d7bfc030526be80383f
vanessarcosta <vanessajreis38@gmail.com>
2024-11-15 00:58:31 +0000

    feat (Exc4For_10MesmaAltura): calcular quando tera mesma altura

---
+++ Exc4For/Exc4For_10MesmaAltura/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exc4For_10MesmaAltura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double alturaJoao = 1.50, alturaZe = 1.10;
+            int i = 0;
+
+            for (; alturaZe < alturaJoao; i++)
+            {
+                alturaJoao += 0.02;
+                alturaZe += 0.03;
+            }
+            
+            Console.WriteLine($"É necessários {i} anos para que Zé seja maior que João.");
+        }
+    }
+}
