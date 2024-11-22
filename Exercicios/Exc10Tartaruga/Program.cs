0ed09cea18d710d59fc0830e706c2439b9ff1439
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:37:49 +0000

    feat (Exc10Tartaruga): calcular escalada

---
+++ Exercicios/Exc10Tartaruga/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.Intrinsics.X86;
+
+namespace Exc10Tartaruga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura = 0, dias = 0;
+
+            do
+            {
+                dias += 1;
+                altura++;
+            }
+            while (altura <= 30);
+
+            Console.WriteLine($"A tartaruga vai gastar {dias} dias para alcançar 30 m.");
+
+        }
+    }
+}
