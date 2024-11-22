e3fa37e3f1df77a449ca21dcb78f7db0665d9b53
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 17:32:09 +0000

    code review (Exc4__6Somatorio): refinamento do codigo, validação de dados

--- Exc4/Exc4_6Somatorio/Program.cs
+++ Exc4/Exc4_6Somatorio/Program.cs
@@ -4,20 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int numero1 = 0, numero2 = 0, aux = 0;
-
-            Console.WriteLine("Digite o primeiro número: ");
-            int.TryParse(Console.ReadLine(), out numero1);
-            Console.WriteLine("Digite o segundo número: ");
-            int.TryParse(Console.ReadLine(), out numero2);
+            int numero1, numero2, aux = 0, soma = 0;
+                        
+            do
+            {
+                Console.Write("Digite o primeiro número: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out numero1));
 
             do
             {
-                numero1++;
-                aux += numero1;                
+                Console.Write("Digite o segundo número: ");
             }
-            while (numero1 < numero2 - 1);
-            Console.WriteLine($"Somatório do intervalo = {aux}");
+            while (!int.TryParse(Console.ReadLine(), out numero2));
+
+            if (numero1 > numero2)
+            {
+                aux = numero1;
+                numero1 = numero2;
+                numero2 = aux;
+            }
+
+            do
+            {
+                soma += numero1;
+                numero1++;
+            }
+            while (numero1 <= numero2);
+
+            Console.WriteLine($"Somatório do intervalo = {soma}");
 
         }
     }
