7ead6d07b25c3c212e46f8f1ca7bf229ff1deb6f
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:36:44 +0000

    feat (Exc9bSomatorioParesMedia): media somatorio num intervalo pares

---
+++ Exercicios/Exc9bSomatorioParesMedia/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exc9bSomatorioParesMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1, somatorio = 0, aux = 0;
+            do
+            {
+                if (numero % 2.0 == 0)
+                {
+                    somatorio += numero;
+                    aux += 1;
+                }
+                numero++;
+            }
+            while (numero < 20);
+            Console.WriteLine($"Somatório do números pares do intervalo 0 de até 20 = {somatorio / aux}");
+
+        }
+    }
+}
