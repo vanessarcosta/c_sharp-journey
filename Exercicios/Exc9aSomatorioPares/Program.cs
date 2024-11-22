bf9c686f5637e23a850a2e3475d0c44f607f26dc
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:35:02 +0000

    feat (Exc9SomatorioPares): somatorio num intervalo pares

---
+++ Exercicios/Exc9aSomatorioPares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Exc9aSomatorioPares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, somatorio = 0;
+            do
+            {
+                if (numero % 2.0 == 0)
+                {
+                    somatorio += numero;
+                }
+                numero++;
+            }
+            while (numero < 20);
+            Console.WriteLine($"Somatório do números pares do intervalo 0 de até 20 = {somatorio}");
+        }
+    }
+}
