f03d97f84ac2196eb47cc32e16d5bd26b426563d
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:31:18 +0000

    feat (Exc7): imprimir numero

---
+++ Exercicios/Exc7PorExtenso/Program.cs
@@ -0,0 +1,64 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Exc7PorExtenso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idioma, numero;
+
+            Console.Write("Qual idioma deseja imprimir? Ingles (digite 1) ou português (digite 2): ");
+            idioma = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite um número entre 1 e 5: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            switch (idioma)
+            {
+                case 1:
+
+                    switch (numero)
+                    {
+                        case 1:
+                            Console.Write("One.");
+                            break;
+                        case 2:
+                            Console.Write("Two.");
+                            break;
+                        case 3:
+                            Console.Write("Three.");
+                            break;
+                        case 4:
+                            Console.Write("Four.");
+                            break;
+                        case 5:
+                            Console.Write("Five.");
+                            break;                    }
+                    break;
+                case 2:
+
+                    switch (numero)
+                    {
+                        case 1:
+                            Console.Write("Um.");
+                            break;
+                        case 2:
+                            Console.Write("Dois.");
+                            break;
+                        case 3:
+                            Console.Write("Três.");
+                            break;
+                        case 4:
+                            Console.Write("Quatro.");
+                            break;
+                        case 5:
+                            Console.Write("Cinco.");
+                        break;
+                    }
+                    break;
+            }
+
+        }
+    }
+    
+}
