d17cd4023f16823fbed67b4d12d4b8d5b6adeaea
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:15:07 +0000

    feat (Exc3Funcao): calcular uma funcao

---
+++ Exercicios/Exc4FuncaoX/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exc4FuncaoX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            Console.Write("Digite o valor de x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if (x <= 1)
+            {
+                x = 1;
+            }
+            else if (x <= 2)
+            {
+                x = 2;
+            }
+            else if (x <= 3)
+            {
+                x = x * x;
+            }
+            else
+            {
+                x = x * x * x;
+            }
+
+            Console.Write($"O valor de f(x) = {x}");
+        }
+    }
+}
