c77c45b053ff8805d59bdadff1193eab53532e2e
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:32:31 +0000

    feat (Exc8Admissao): verificar requisitos admissao funcionario

---
+++ Exercicios/Exc8Admissao/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Exc8Admissao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int notaFis, notaQui, notaMat, somaTres = 0, somaMatFis = 0;
+            
+            do
+            {
+                Console.Write("Digite a nota de Física: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out notaFis));
+
+            do
+            {
+                Console.Write("Digite a nota de Química: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out notaQui));
+
+            do
+            {
+                Console.Write("Digite a nota de Matemática: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out notaMat));
+
+            somaTres = notaFis+ notaMat + notaQui;
+            somaMatFis = notaMat + notaFis;
+
+            if (notaFis >= 65 && notaFis >=55 && notaQui >= 50 && (somaTres >= 180 || somaMatFis >= 140))
+            {
+                Console.WriteLine("O candidato foi admitido!");
+            }
+            else
+            {
+                Console.WriteLine("O candidato não foi admitido.");
+            }
+
+        }
+    }
+}
