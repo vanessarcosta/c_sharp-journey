91f0dfc77299537285a422dc23c9a7cc438475a2
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:13:46 +0000

    feat (Exc3Raizes): calcular raizes segundo grau

---
+++ Exercicios/Exc3Raizes/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exc3Raizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, primeiraRaiz, segundaRaiz;
+
+            Console.Write("Digite o valor de a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor de b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor de c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            primeiraRaiz = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+            segundaRaiz = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+
+            Console.Write($"As raizes da equação são: {primeiraRaiz}, {segundaRaiz}");
+        }
+    }
+}
