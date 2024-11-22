5830aef2a9a5d16644bab3b6e08d02316788b121
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:16:45 +0000

    feat (Exc3converterKmMilhas): converter medidas

---
+++ Exercicios/Exc5converterKmMIilhas/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Exc5converterKmMIilhas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha;
+            double km, milhas;obrigada
+
+            do
+            {
+                Console.Write("Deseja converter: Km pra milhas (digite 1) ou milhas para km (digite 2)?");
+            }
+            while (!int.TryParse(Console.ReadLine(), out escolha));
+
+            if (escolha == 1)
+            {
+                do
+                {
+                    Console.Write("Informe o valor em quilômetros: ");
+                  
+                }
+                while (!double.TryParse(Console.ReadLine(), out km)); 
+
+                    milhas = km * 0.621371;
+                Console.WriteLine($"{km} Km equivale {Math.Round(milhas, 2)} milhas.");
+            }
+            else if (escolha == 2) 
+            {
+                do
+                {
+                    Console.Write("Informe o valor em milhas: ");
+                }
+                while (!double.TryParse(Console.ReadLine(), out milhas));
+                km = milhas * 1.60934;
+                Console.WriteLine($"{milhas} milhas equivale {Math.Round(km,2)} Km.");
+            }
+            else
+            {
+                Console.Write("Valor inválido!");
+            }
+           
+        }
+    }
+}
