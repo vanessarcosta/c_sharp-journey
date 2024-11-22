8be3b846ed93573caf130aa6e59f0783ed4b5acd
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:40:08 +0000

    feat (Exc12): calcular expressao

---
+++ Exercicios/Exc12/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exc12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double N, A = 0;
+            int aux = 1;
+
+            do
+            {
+                Console.Write("Insira o valor de N: ");
+            } while (!double.TryParse(Console.ReadLine(), out N));
+
+            while (N >= 1)
+            {
+                A = A + N / aux;
+                aux++;
+                N--;
+            }
+
+            A = A + N / aux;
+
+            Console.Write($"A possui o valor: {A}");
+        }
+    }
+}
