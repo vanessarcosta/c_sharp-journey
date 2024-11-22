b2b463c94cc71996f2396a045fddc0bd3f715ec6
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 20:18:21 +0000

    feat (Exc4_12DigitoConDemo): calcular digito

---
+++ Exc4/Exc4_12DigitoConDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exc4_12DigitoConDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, numero, dig = 1, total = 0, nat = 1, checkDigito = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Insira um número: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            n = numero;
+
+            while( n > 0)
+            {
+                dig = n % 10;
+                total += dig * nat;
+                n /= 10;
+                nat++;
+            }
+
+            checkDigito = total % 7;
+
+            Console.WriteLine($"O código com digito de controlo é {numero}{checkDigito}.");
+        }
+    }
+}
