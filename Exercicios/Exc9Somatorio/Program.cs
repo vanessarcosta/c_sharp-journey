948c925feeed6e9244ce28f960fc792e19943f3c
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:33:44 +0000

    feat (Exc9Somatorio): somatorio num intervalo

---
+++ Exercicios/Exc9Somatorio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Exc9Somatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, somatorio = 0;
+            do
+            {
+                numero++;
+                somatorio += numero;
+            }
+            while (numero < 20);
+            Console.WriteLine($"Somatório do intervalo 0 de até 20 = {somatorio}");
+        }
+    }
+}
