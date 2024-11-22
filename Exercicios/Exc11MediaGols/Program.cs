16e2567dad96421be0741d21dd7f7b52ce315c32
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:38:46 +0000

    feat (Exc11MediaGols): calcular media

---
+++ Exercicios/Exc11MediaGols/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exc11MediaGols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double media = 0;
+            double golsSofridos = 0, totalDeGols = 0, aux = 0;
+            int partida = 1;
+
+            while (golsSofridos != 3)
+            { 
+                do
+                {
+                    Console.WriteLine($"Quantos golos o Sporting sofreu na partida: ");
+
+                } while (!double.TryParse(Console.ReadLine(), out golsSofridos));
+
+                partida++;
+                totalDeGols += golsSofridos;
+
+            }
+
+            media = totalDeGols / partida;
+
+            Console.WriteLine($"A média de golos sofridos pelo Sporting é: {media}");
+        }
+    }
+}
