0ec35704b632aba3073de07f3523a425b575bbd4
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 18:58:19 +0000

    code review (Exc4__11Pesquisa): refinamento do codigo

--- Exc4/Exc4_11Pesquisa/Program.cs
+++ Exc4/Exc4_11Pesquisa/Program.cs
@@ -14,19 +14,20 @@ class Program
         {
             do
             {
-                Console.WriteLine("Informe a idade: ");
+                Console.Write("Informe a idade: ");
             }
              while (!int.TryParse(Console.ReadLine(), out idade));
 
             do
             {
-                Console.WriteLine("Informe o sexo (M/F)");
+                Console.Write("Informe o sexo (M/F): ");
+                char.TryParse(Console.ReadLine().ToUpper(), out sexo);
             }
-            while (!char.TryParse(Console.ReadLine(), out sexo));
+            while (!(sexo =='M') && !(sexo == 'F'));
 
             do
             {
-                Console.WriteLine("Informe o salário: ");
+                Console.Write("Informe o salário: ");
             }
             while (!double.TryParse(Console.ReadLine(), out salario));    
 
@@ -43,21 +44,21 @@ class Program
             totalHabitantes++;
             totalSalario += salario;
 
-            if (sexo == 'F' || sexo == 'f' && salario <= 500.0)
+            if (sexo == 'F' && salario <= 500.0)
             {
                 qtdMulher++;
             }        
 
             do
             {
-                Console.WriteLine("Continuar a inserir dados (S/N)");
+                Console.Write("\n\nContinuar a inserir dados (S/N): ");
             }
             while (!char.TryParse(Console.ReadLine(), out op));
 
         }
         while (op == 's' || op == 'S');
 
-        Console.WriteLine($"A média de salário do grupo: {totalSalario / totalHabitantes}.");
+        Console.WriteLine($"\n\nA média de salário do grupo: {totalSalario / totalHabitantes:N2}.");
         Console.WriteLine($"A maior idade é {maiorIdade}, e a menor idade é {menorIdade}.");
         Console.WriteLine($"Quantidade de mulheres com salário até 500 euros: {qtdMulher}.");
 
