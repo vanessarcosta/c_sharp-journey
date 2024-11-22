b3f96f0ea5d07b823081b52e832eee85b1d76702
vanessarcosta <vanessajreis38@gmail.com>
2024-11-22 16:30:06 +0000

    feat (Exc6): calcular valor maximo prestacao

---
+++ Exercicios/Exc6/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exc6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioBruto, prestacao, prestacaoMaxima = 0;
+
+            do
+            {
+                Console.Write("Digite o salario bruto: ");
+            }
+            while (!double.TryParse(Console.ReadLine(), out salarioBruto));
+
+            do
+            {
+                Console.Write("Digite o valor da prestação pretendida: ");
+            }
+            while (!double.TryParse(Console.ReadLine(), out prestacao));
+
+            prestacaoMaxima = salarioBruto * 0.3;
+
+            if (prestacao <= prestacaoMaxima)
+            {
+                Console.WriteLine($"O crédito concedido.");
+            }
+            else
+            {
+                Console.WriteLine("O crédito não pode ser concedido.");
+            }
+        }
+    }
+}
