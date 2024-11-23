554d970347159d7a0d51b62ea526729c03b9b023
vanessarcosta <vanessajreis38@gmail.com>
2024-11-23 00:39:01 +0000

    feat (Exc1LerVetor): implementado leitura do vetor

---
+++ Exc6/Exc1LerVetor/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exc1LerVetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidade;
+            do
+            {
+                Console.Write("Qual o tamanho do vetor: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out quantidade));
+
+            int[] numeros = new int[quantidade];
+            
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                do
+                {
+                    Console.Write($"Insira o número {i + 1}: ");
+                }
+                while (!int.TryParse(Console.ReadLine(),out numeros[i]));
+            }
+        }
+    }
+}
