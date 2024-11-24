d17d37a015739e30b215b773bca819b482a4f86f
vanessarcosta <vanessajreis38@gmail.com>
2024-11-24 14:47:12 +0000

    feat (Exc4aSomatorio): implementado somatorio

---
+++ Exc6/Exc4aSomatorio/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exc4aSomatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidade, soma = 0;
+            do
+            {
+                Console.Write("Qual o tamanho do vetor: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out quantidade));
+
+            int[] numeros = new int[quantidade];
+
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                do
+                {
+                    Console.Write($"Insira o número {i + 1}: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out numeros[i]));
+            }
+
+            for (int j = 0; j < numeros.Length; j++)
+            {
+                soma += numeros[j];
+            }
+
+            Console.WriteLine($"O somatório de todos os elementos do array = {soma}");
+        }
+    }
+}
