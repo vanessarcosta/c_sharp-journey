56cb030dd9e4fc363053eab615037b70f76fe465
vanessarcosta <vanessajreis38@gmail.com>
2024-11-24 15:08:39 +0000

    feat (Exc5MaiorMenor): verificar menor e maior valor

---
+++ Exc6/Exc5MaiorMenor/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exc5MaiorMenor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidade;
+            do
+            {
+                Console.Write("Qual o tamanho do vetor: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out quantidade));
+
+            int[] numeros = new int[quantidade];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                do
+                {
+                    Console.Write($"Insira o número {i + 1}: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out numeros[i]));
+            }
+
+            int maiorElemento = numeros[0], menorElemento = numeros[0];
+
+            for (int j = 0; j < numeros.Length; j++)
+            {
+                if (numeros[j] > maiorElemento)
+                    maiorElemento = numeros[j];
+
+                if (numeros[j] < menorElemento)
+                    menorElemento = numeros[j];
+
+            }
+
+            Console.WriteLine($"O maior elementos do array = {maiorElemento}");
+            Console.WriteLine($"O menor elementos do array = {menorElemento}");
+
+        }
+    }
+}
