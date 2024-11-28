75711d8306b1f09678af1122167adfc9bfe85afb
vanessarcosta <vanessajreis38@gmail.com>
2024-11-28 21:02:39 +0000

    code review /feat (Exc1Enfermaria):implementado calculo maior valor, refinamento do codigo

--- Exc7/Exc1Enfermaria/Program.cs
+++ Exc7/Exc1Enfermaria/Program.cs
@@ -4,37 +4,40 @@
     {
         static void Main(string[] args)
         {
-            int[,] enfermaria = new int[3, 2];
-            
+            int[,] enfermaria = new int[24,4];
 
-            for (int i = 0; i < 3; i++)
+
+            for (int i = 0; i < 24; i++)
             {
-                for (int j = 0; j < 2; j++)
-                {
-                    
-                    do
-                        {
-                            Console.WriteLine($"Insira a pulsação do paciente da {i}: hora(s) ");
-                            Console.Write($"Paciente do leito {j + 1}: \n");
+                Console.WriteLine($"{i} hora(s). Insira a pulsação:");
 
-                        } while (!int.TryParse(Console.ReadLine(), out enfermaria[i, j]));
-             
+                for (int j = 0; j < 4; j++)
+                {
+                    do
+                    {                        
+                        Console.Write($"Paciente do leito {j + 1}: ");
+                    } while (!int.TryParse(Console.ReadLine(), out enfermaria[i, j]));
                 }
+                Console.WriteLine();
             }
 
+            Console.WriteLine("Pressione qualquer tecla para verificar resultados...");
+            Console.ReadKey();
+            Console.Clear();
+
             double totalPulsacao = 0;
-            double[] mediaPulsacoes = new double[2];
-            
-            for (int j = 0; j < 2; j++)
+            double[] mediaPulsacoes = new double[4];
+
+            for (int j = 0; j < 4; j++)
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 24; i++)
                 {
                     totalPulsacao += enfermaria[i, j];
                 }
 
-                Console.WriteLine($"A média de pulsação do pa ciente do leito {j+1} ="+
-                   $"\n {totalPulsacao/3:N2}"); 
-                mediaPulsacoes[j] = totalPulsacao/3;
+                Console.WriteLine($"A média de pulsação do paciente do leito {j + 1}:" +
+                   $" {totalPulsacao / 3:N2}\n");
+                mediaPulsacoes[j] = totalPulsacao / 24;
                 totalPulsacao = 0;
             }
 
@@ -49,8 +52,27 @@
                     posicaoLeito = i;
                 }
             }
-            Console.WriteLine($"O paciente do leito {posicaoLeito +1} apresentou a maior valor médio =" +
-                $"{maiorValor:N2} de pulsações entre os leitos.");
+            Console.WriteLine($"O paciente do leito {posicaoLeito + 1} apresentou o maior " +
+                $"valor médio entre os leitos com {maiorValor:N2} pulsações. \n");
+
+            int maiorPulsacao = 0, leito =0, hora = 0;
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if(enfermaria[i,j] > maiorPulsacao)
+                    {
+                        maiorPulsacao = enfermaria[i,j];
+                        leito = j;
+                        hora = i;
+                    }
+
+                }
+            }
+
+            Console.WriteLine($"O paciente do leito {leito+ 1} apresentou às {hora}hora(s) " +
+            $"a maior pulsação entre os leitos com {maiorPulsacao} pulsações.\n");
+
         }
     }
 }
