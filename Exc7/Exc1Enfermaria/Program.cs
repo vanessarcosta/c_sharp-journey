042bef2a3d98ac8a9f230892ccecdd350442f95b
vanessarcosta <vanessajreis38@gmail.com>
2024-11-28 18:30:51 +0000

    feat (Exc1Enfermaria): implementado leitura e amazenamento

--- Exc7/Exc1Enfermaria/Program.cs
+++ Exc7/Exc1Enfermaria/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[,] golos = new int[3, 2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"Insira a pulsação do paciente da {i}: hora(s) ");
+                    do
+                        {
+                            Console.Write($"Paciente do leito {j + 1}: ");
+
+                        } while (!int.TryParse(Console.ReadLine(), out golos[i, j]));
+             
+                }
+            }
         }
     }
 }
