bd8556a3318cdda56b12c3253f2d92b2ebed71d0
vanessarcosta <vanessajreis38@gmail.com>
2024-11-28 18:03:58 +0000

    feat (Demo2): implementado media e pontuação

--- DemoMatrizes/Demo2/Program.cs
+++ DemoMatrizes/Demo2/Program.cs
@@ -40,6 +40,43 @@
                 Console.WriteLine($"Jornada {i + 1} - Marcados: {golos[i, 0]} Sofridos: {golos[i, 1]}");
             }
 
+            Console.WriteLine("Pressione qualquer tecla para ver a estatística...");
+            Console.ReadKey();
+
+            int totalMarcados = 0, totalSofridos = 0;
+
+            for (int i = 0;i < 6;i++)
+            {
+                totalMarcados += golos[i, 0];
+                totalSofridos += golos[i, 1];
+            }
+
+            Console.WriteLine("************************ESTATÍSTICA************************");
+            Console.WriteLine($"Total de Golos Marcados: {totalMarcados}");
+            Console.WriteLine($"Total de Golos Marcados: {totalSofridos}");
+
+            Console.WriteLine("Pressione qualquer tecla para ver a pontuação...");
+            Console.ReadKey();
+
+            //Vitória -> 3 pontos
+            //Empate -> 1 ponto
+            //Derrota -> 0 pontos
+
+            int pontos = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (golos[i,0] > golos[i,1])
+                {
+                    pontos += 3;
+                }
+                else if (golos[i,0] == golos[i,1])
+                {
+                    pontos += 1;
+                }
+            }
+            Console.WriteLine("************************PONTUAÇÃO************************");
+            Console.WriteLine($"Total de Pontos: {pontos}");
         }
     }
 }
