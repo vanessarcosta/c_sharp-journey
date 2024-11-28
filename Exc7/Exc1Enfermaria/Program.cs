de171fe37897df34d5855d84fa113f3e7653338e
vanessarcosta <vanessajreis38@gmail.com>
2024-11-28 19:33:35 +0000

    feat (Exc1Enfermaria): implementado calculo media

--- Exc7/Exc1Enfermaria/Program.cs
+++ Exc7/Exc1Enfermaria/Program.cs
@@ -4,20 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int[,] golos = new int[3, 2];
+            int[,] enfermaria = new int[3, 2];
+            
 
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 2; j++)
                 {
-                    Console.WriteLine($"Insira a pulsação do paciente da {i}: hora(s) ");
+                    
                     do
                         {
-                            Console.Write($"Paciente do leito {j + 1}: ");
+                            Console.WriteLine($"Insira a pulsação do paciente da {i}: hora(s) ");
+                            Console.Write($"Paciente do leito {j + 1}: \n");
 
-                        } while (!int.TryParse(Console.ReadLine(), out golos[i, j]));
+                        } while (!int.TryParse(Console.ReadLine(), out enfermaria[i, j]));
              
                 }
+            }
+
+            int totalPulsacao = 0;
+            
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    totalPulsacao += enfermaria[i, j];
+                }
+                Console.WriteLine($"A média de pulsação do paciente do leito {j+1} ="+
+                   $"\n {totalPulsacao/3:N2}");
+                totalPulsacao = 0;
             }
         }
     }
