09cb23fc3ff359c493101c6c27393b84019c4168
vanessarcosta <vanessajreis38@gmail.com>
2024-11-28 21:27:25 +0000

    feat (Exc2):implementado ler, armazenar e verificar maior elemento matriz

---
+++ Exc7/Exc2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exc2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[3,3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    do
+                    {
+                        Console.Write($"Insira os valores na matriz: ");
+                    } while (!int.TryParse(Console.ReadLine(), out numeros[i, j]));
+                }
+            }
+
+            int maiorElemento = 0;
+
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (numeros[i, j] > maiorElemento)
+                        maiorElemento = numeros[i, j];   
+                }
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para prosseguir...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine($"O maior elemento da matriz é {maiorElemento}");
+
+        }
+    }
+}
