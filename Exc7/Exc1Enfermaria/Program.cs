54e615cb2008c7e1830a13da7ae09b2c573a4083
vanessarcosta <vanessajreis38@gmail.com>
2024-11-28 18:04:53 +0000

    config(Exc7): config pasta solution

---
+++ Exc7/Exc1Enfermaria/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exc1Enfermaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
