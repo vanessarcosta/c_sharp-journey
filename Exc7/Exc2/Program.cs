e7c96b442a25e7a524abe21a23ffb8d05f625476
vanessarcosta <vanessajreis38@gmail.com>
2024-11-29 00:34:50 +0000

    feat (Exc2):implementado calcular soma de toda matriz e troca linha por coluna

--- Exc7/Exc2/Program.cs
+++ Exc7/Exc2/Program.cs
@@ -4,33 +4,24 @@
     {
         static void Main(string[] args)
         {
-            int[,] numeros = new int[3,3];
+            int[,] numeros = new int[10,10];
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < 10; j++)
                 {
                     do
                     {
                         Console.Write($"Insira os valores na matriz: ");
-                    } while (!int.TryParse(Console.ReadLine(), out numeros[i, j]));
+                    } while (!int.TryParse(Console.ReadLine(), out numeros[i,j]));
                 }
-            }
-
-            for (int i = 0; i < 3; i++) // Itera pelas linhas
-            {
-                for (int j = 0; j < 3; j++) // Itera pelas colunas
-                {
-                    Console.Write(numeros[i, j] + " ");
-                }
-                Console.WriteLine(); // Pula para a próxima linha
             }
 
             int maiorElemento = 0;
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < 10; j++)
                 {
                     if (numeros[i, j] > maiorElemento)
                         maiorElemento = numeros[i, j];   
@@ -44,22 +35,21 @@
             Console.WriteLine($"O maior elemento da matriz é {maiorElemento}\n");
 
             double totalCol = 0;
-            
             int k = 1;
-            for (int i = 0; i < 3; i++)
+
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0;j < 3; j++)
+                for (int j = 0;j < 10; j++)
                 {
                     totalCol += numeros[j,i];
-                    
                 }
-                Console.WriteLine($"A média dos elementos da coluna {k} é {totalCol / 3:N2}\n");
+                Console.WriteLine($"A média dos elementos da coluna {k} é {totalCol / 10:N2}\n");
                 k++;
                 totalCol = 0;
             }
 
             int somaLin3 = 0;
-            for (int j =0; j < 3; j++)
+            for (int j =0; j < 10; j++)
             {
                 somaLin3 += numeros[2,j];
             }
@@ -67,6 +57,7 @@
             Console.WriteLine($"A soma dos elementos da linha 3 é {somaLin3}\n");
 
             int col= 0;
+
             do
             {
                 Console.Write($"Escolha uma coluna: ");
@@ -74,18 +65,51 @@
 
             int somaLin = 0;
             col -= 1;
-            for (int j = 0; j < 3; j++)
+
+            for (int j = 0; j < 10; j++)
             {
                 somaLin += numeros[j,col];
             }
             Console.WriteLine($"A soma dos elementos da coluna {col+1} é {somaLin}\n");
 
             int somaDiagonal = 0;
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 10; i++)
             {
                 somaDiagonal += numeros[i,i];
             }
             Console.WriteLine($"A soma dos elementos da diagonal principal é {somaDiagonal}\n");
+
+            int somaTotal = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                   somaTotal += numeros[i,j];
+                }
+            }
+
+            Console.WriteLine($"O número de elementos da matriz é {numeros.Length}");
+
+            int[,] matriz_2 = new int[10,10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    matriz_2[i,j] =numeros[j,i];
+                }
+            }
+
+            Console.WriteLine("A nova matriz:");
+
+            for (int i = 0; i < 10; i++) 
+            {
+                for (int j = 0; j < 10; j++) 
+                {
+                    Console.Write(matriz_2[i, j] + " ");
+                }
+                Console.WriteLine(); 
+            }
+
         }
     }
 }
