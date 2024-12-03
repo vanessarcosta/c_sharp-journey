7ed939a11be3e877b673472e5799e6bc338b611c
vanessarcosta <vanessajreis38@gmail.com>
2024-12-03 19:59:25 +0000

    feat (Exc1RaizesQuadradas): implementado leitura e validação

--- Exc8/Exc1RaizesQuadradas/Program.cs
+++ Exc8/Exc1RaizesQuadradas/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
+            int Inf, Sup;
 
+            Inf = PedirNumeroInf();
+            Sup = PedirNumeroSup();
         }
+
+        static int PedirNumeroSup()
+        {
+            int numero;
+
+            do
+            {
+                Console.Write("Insira o primeiro número: ");
+            }
+
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            return numero;
+        }
+
+
+        static int PedirNumeroInf()
+        {
+            int numero;
+
+            do
+            {
+                Console.Write("Insira o primeiro número: ");
+            }
+
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            return numero;
+        }
+
     }
 }
