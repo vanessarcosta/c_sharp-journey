40ffaed28ebae5ddd88b94fb5db339380e962a75
vanessarcosta <vanessajreis38@gmail.com>
2024-12-04 22:08:17 +0000

    feat (Exc4): metodo trocar variavel

---
+++ Exc8/Exc4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exc4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeiraString, segundaString;
+
+            Console.Write($"Insira o primeiro conjunto de caracteres: ");
+            primeiraString = Console.ReadLine();
+
+            Console.Write($"Insira o segundo conjunto de caracteres: ");
+            segundaString = Console.ReadLine();
+
+            TrocarStrings(ref primeiraString, ref segundaString);
+
+            Console.WriteLine("\nTROCA O CONJUNTO DE CARACTERES");
+            Console.WriteLine($"Primeiro conjunto de caracteres: {primeiraString}.");
+            Console.WriteLine($"Segundo conjunto de caracteres: {segundaString}");
+        }
+   
+        static void TrocarStrings(ref string primeiroParametro, ref string segundoParametro)
+        {
+            string aux = primeiroParametro;
+            primeiroParametro = segundoParametro;
+            segundoParametro = aux;
+        }
+    }
+}
