f369169e601437ec8aba03ad1466ea7aaf45bdb6
vanessarcosta <vanessajreis38@gmail.com>
2024-12-04 09:46:05 +0000

    feat (Exc1RaizesQuadradas): implementado leitura e armazenamento de dados

---
+++ Exc8/Exc3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exc3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+
+            l = TamanhoDoVetor();
+
+            string[] nomes = new string[l];
+
+            PreencheTabela(nomes);
+
+        }
+
+        static void PreencheTabela(string[] nomes)
+        {
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write($"Insira o nome {i + 1}: ");
+                nomes[i] = Console.ReadLine();
+            }
+        }
+        static int TamanhoDoVetor()
+        {
+            int numero;
+
+            do
+            {
+                Console.Write("Quantos nomes deseja digitar? ");
+            }
+
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            return numero;
+        }
+
+    }
+}
