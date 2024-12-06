c82c903490ba7dd047f2bd9cefccef92f9b820f6
vanessarcosta <vanessajreis38@gmail.com>
2024-12-06 15:54:47 +0000

    feat (Exc6): converte segundos utilizando metodos

---
+++ Exc8/Exc6/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Exc6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int segundos, minutos, horas =0;
+
+            segundos = PedeNumero();
+
+            (minutos,segundos) = ConverteMinutosEHoras(ref segundos);
+
+            if (minutos > 60) 
+            { 
+                (horas,minutos) = ConverteMinutosEHoras(ref  minutos);
+            }
+            Console.WriteLine($"O valor em segundos equivale a {horas} horas, {minutos} minutos e {segundos} secundos.");
+
+        }
+        static (int, int) ConverteMinutosEHoras( ref int numero)
+        {
+            int resto = 0;
+            if (numero % 60 != 0)
+            {
+                resto = numero % 60;
+            }
+            numero = numero / 60;
+            return (numero, resto);
+
+        }
+        static int PedeNumero()
+        {
+            int numero;
+
+            do
+            {
+                Console.Write("Digite o valor em segundo para converter: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            return numero;
+        }
+    }
+}
