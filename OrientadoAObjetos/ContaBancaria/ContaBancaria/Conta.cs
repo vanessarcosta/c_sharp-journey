15eeac22470e10a0a5adaad7a384162a7596226c
vanessarcosta <vanessajreis38@gmail.com>
2025-02-13 21:33:48 +0000

    feat (ContaBancaria): implementado a classe conta

---
+++ OrientadoAObjetos/ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public int Numero {get; private set;}
+        public double DepositoInicial { get; private set;}
+        public double Saldo {get; private set;}
+
+        string titular;
+        public string Titular
+        {
+            get
+            {
+                return titular;
+            }
+            set
+            {
+                if (titular != null && value.Length > 1)
+                { titular = value; }
+            }
+        }
+
+        #region Methods
+        #endregion
+    }
+}
