d7e3e748d7e6b8c10e49a30ff74d242c1054a9ef
vanessarcosta <vanessajreis38@gmail.com>
2025-02-14 17:59:08 +0000

    feat (ContaBancaria): implementado outras funcionalidades

--- OrientadoAObjetos/ContaBancaria/ContaBancaria/Conta.cs
+++ OrientadoAObjetos/ContaBancaria/ContaBancaria/Conta.cs
@@ -4,8 +4,8 @@ namespace ContaBancaria
 {
     class Conta
     {
-        public int Numero {get; }
-        public double DepositoInicial { get; private set;}
+        public int Numero { get; set; }
+        public double DepositoInicial { get; set;}
         public double Saldo {get; private set;}
         public double TaxaDeSaque  { get; }
         public double Valor { get; set; }
@@ -19,13 +19,14 @@ namespace ContaBancaria
             }
             set
             {
-                if (titular != null && value.Length > 1)
-                { titular = value; }
+                if (!string.IsNullOrEmpty(value) && value.Length > 1)
+                 titular = value;
             }
         }
         public Conta() 
         {
             TaxaDeSaque = 5.00;
+            Saldo = 0.00;
         }
 
         public Conta(int numero, string titular, double depositoIncial, double saldo)
@@ -48,15 +49,16 @@ namespace ContaBancaria
 
         public double RealizarSaque(double saldoAnterior, double valor)
         {
-            Saldo = saldoAnterior - valor;
-            return Saldo - TaxaDeSaque;
+            Saldo = saldoAnterior - valor - TaxaDeSaque;
+            return Saldo;
         }
         public override string ToString()
         {
             return "Conta " + Numero
-                + ", Titular: " + Titular 
+                + ", Titular: "
+                + Titular 
                 + ", Saldo: $"
-                + Saldo.ToString("F2", CultureInfo.InvariantCulture;
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
         }
         #endregion
     }
--- OrientadoAObjetos/ContaBancaria/ContaBancaria/Program.cs
+++ OrientadoAObjetos/ContaBancaria/ContaBancaria/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace ContaBancaria
+﻿using System.Globalization;
+using ContaBancaria;
+
+namespace ContaBancaria
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string escolhaDepositoIncial;
+
+            Conta conta = new Conta();
+
+            Console.Write("Entre o número da conta: ");
+            conta.Numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            conta.Titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito incial (s/n)?");
+            escolhaDepositoIncial = Console.ReadLine();
+
+            if (escolhaDepositoIncial == "s")
+            {
+                Console.Write("Entre o valor de depósito incial: ");
+                conta.DepositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.RealizarDeposito(conta.Saldo, conta.DepositoInicial);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta.ToString());
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            conta.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.RealizarDeposito(conta.Saldo, conta.Valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta.ToString());
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            conta.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.RealizarSaque(conta.Saldo, conta.Valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta.ToString());
+
+
 
         }
     }
