49ba930bd540dbf9fd6dc994105ef2433932f344
vanessarcosta <vanessajreis38@gmail.com>
2025-02-14 16:17:01 +0000

    feat (ContaBancaria): implementado métodos e construtores

--- OrientadoAObjetos/ContaBancaria/ContaBancaria/Conta.cs
+++ OrientadoAObjetos/ContaBancaria/ContaBancaria/Conta.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace ContaBancaria
 {
     class Conta
     {
-        public int Numero {get; private set;}
+        public int Numero {get; }
         public double DepositoInicial { get; private set;}
         public double Saldo {get; private set;}
+        public double TaxaDeSaque  { get; }
+        public double Valor { get; set; }
 
         string titular;
         public string Titular
@@ -25,8 +23,41 @@ namespace ContaBancaria
                 { titular = value; }
             }
         }
+        public Conta() 
+        {
+            TaxaDeSaque = 5.00;
+        }
 
+        public Conta(int numero, string titular, double depositoIncial, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = depositoIncial;
+            TaxaDeSaque = 5.00;
+        }
         #region Methods
+        public void AlterarNome(string titularAtualizado)
+        {
+            Titular = titularAtualizado;
+        }
+
+        public void RealizarDeposito(double saldoAnterior, double valor)
+        {
+            Saldo = saldoAnterior + valor;
+        }
+
+        public double RealizarSaque(double saldoAnterior, double valor)
+        {
+            Saldo = saldoAnterior - valor;
+            return Saldo - TaxaDeSaque;
+        }
+        public override string ToString()
+        {
+            return "Conta " + Numero
+                + ", Titular: " + Titular 
+                + ", Saldo: $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture;
+        }
         #endregion
     }
 }
