a94a605852e92d8e90fe717aac1d5a521f41fbfe
vanessarcosta <vanessajreis38@gmail.com>
2025-02-17 23:42:06 +0000

    feat (Course): demo Params

---
+++ OrientadoAObjetos/Course/DemoParams/Calculator.cs
@@ -0,0 +1,41 @@
+﻿namespace DemoParams
+{
+    class Calculator
+    {
+        //***solução com uso de sobrecarga !! solução ruim!!
+        //public static int Sum(int n1, int n2)
+        //{
+        //    return n1 + n2;
+        //}
+        //public static int Sum(int n1, int n2, int n3)
+        //{
+        //    return n1 + n2 + n3;
+        //}
+        //public static int Sum(int n1, int n2, int n3, int n4)
+        //{
+        //    return n1 + n2 + n3 + n4;
+        //}
+
+        ////***solução com vetor**  muito verbosa a instanciação de vetores na Main!!
+        ////public static int Sum(int[] numbers)
+        ////{
+        ////    int sum = 0;
+        ////    for (int i = 0; i < numbers.Length; i++)
+        ////    {
+        ////        sum += numbers[i];
+        ////    }
+        ////    return sum;
+        ////}
+        ///
+
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ OrientadoAObjetos/Course/DemoParams/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DemoParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //chamada com sobrecarga
+            ////int s1 = Calculator.Sum(2, 3);
+            ////int s2 = Calculator.Sum(2, 4, 3);
+
+            //chamada com vetor
+            ////int s1 = Calculator.Sum(new int[] { 2, 3 });
+            ////int s2 = Calculator.Sum(new int[] { 2, 4, 3});
+
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 4, 3);
+            
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+           //int result = Calculator.Sum(new int[] { 10, 20, 30, 40 });
+           // Console.WriteLine(result);
+        }
+    }
+}
