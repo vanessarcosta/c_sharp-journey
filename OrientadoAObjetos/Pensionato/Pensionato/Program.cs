2d941aaec43765d024b8fa0a44b36351d875a050
vanessarcosta <vanessajreis38@gmail.com>
2025-02-17 23:03:41 +0000

    code review (Pensionato): refinamento do codigo

--- OrientadoAObjetos/Pensionato/Pensionato/Program.cs
+++ OrientadoAObjetos/Pensionato/Pensionato/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
 
 namespace Pensionato
 {
@@ -8,24 +9,24 @@ namespace Pensionato
         static void Main(string[] args)
         {
 
-            PensionatoService[] quartos = new PensionatoService[9];
+            Estudante[] quartos = new Estudante[10];
 
             Console.Write("Quantos quartos serão alugados? ");
             int n = int .Parse(Console.ReadLine());
-            Console.WriteLine();
 
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine($"Aluguel #{i+1}");
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i + 1}");
                 Console.Write("Nome: ");
                 string name = Console.ReadLine();
                 Console.Write("Email: ");
                 string email = Console.ReadLine();
                 Console.Write("Quarto: ");
-                int quarto = int .Parse(Console.ReadLine());
+                int quarto = int.Parse(Console.ReadLine());
 
-                quartos[quarto] = new PensionatoService { Name = name, Email = email};
-                Console.WriteLine();
+                quartos[quarto] = new Estudante(name, email);
+               
             }
 
             Console.WriteLine();
@@ -34,7 +35,7 @@ namespace Pensionato
             for (int i = 0;i < quartos.Length; i++)
             {
                 if (quartos[i] != null)
-                    Console.WriteLine(i + ": " + quartos[i].Name + ", " + quartos[i].Email);
+                    Console.WriteLine(i + ": " + quartos[i]);
             }                        
             
         }
