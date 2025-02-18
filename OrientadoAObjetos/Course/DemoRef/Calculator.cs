effb7d0f7916648d33bb943f311757740449b228
vanessarcosta <vanessajreis38@gmail.com>
2025-02-18 00:18:27 +0000

    feat (Course): demo ref e out

---
+++ OrientadoAObjetos/Course/DemoRef/Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace DemoRef
+{
+    class Calculator
+    {
+        //solução que não funciona
+        ////public static void Triple(int x)
+        ////{
+        ////    x = x * 3;
+
+        ////    //Essa solução não funciona pq quando a função for chamada se cria um scope provisório e executa 10*3= 30 
+        ////    // o 30 fica salvo em x e quando terminar e voltar pro Main a váriavel "a" não terá sido alterada 
+        ///     //para resolver deve-ser acrescentar ref na função e na chamada
+        ////}
+        ///
+
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+
+        public static void Quadruple(int origin, out int result)
+        {
+            result = origin * 4;
+        }
+    }
+}
---
+++ OrientadoAObjetos/Course/DemoRef/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DemoRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =10;
+
+            ////chamada sem ref
+            //Calculator.Triple(a);
+            
+            //chamada com ref
+            Calculator.Triple(ref a);
+
+            Console.WriteLine(a);
+
+            int b = 10;
+            int quadruple;
+            Calculator.Quadruple(b, out quadruple);
+            Console.WriteLine(quadruple);
+        }
+    }
+}
