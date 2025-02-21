7bd700ce02b6db241abcbc0e04319352ef6b2e6a
vanessarcosta <vanessajreis38@gmail.com>
2025-02-21 23:01:38 +0000

    feat (UpdateEmployessSalary): create employee class and console application

---
+++ OrientadoAObjetos/UpdateEmployeesSalary/UpdateEmployeesSalary/Employess.cs
@@ -0,0 +1,20 @@
+﻿namespace UpdateEmployeesSalary
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+
+        public Employee(int id, string name, string salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return "Id: " + Id + ", " + Name + ", " + Salary + " €.";
+        }
+    }
+}
---
+++ OrientadoAObjetos/UpdateEmployeesSalary/UpdateEmployeesSalary/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace UpdateEmployeesSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ids = new List<int>();
+            List<string> names = new List<string>();
+            List<double> salaries = new List<double>();
+
+            Console.Write(" How many employess will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine($"Employee #{i + 1}");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                ids.Add(id);
+                names.Add(name);
+                salaries.Add(salary);
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
