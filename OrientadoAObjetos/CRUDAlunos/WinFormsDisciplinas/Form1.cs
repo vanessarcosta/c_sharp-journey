4e6b76d66c22629e6e21c46602d92ae4156a531d
vanessarcosta <vanessajreis38@gmail.com>
2025-02-26 19:28:29 +0000

    feat (CrudAluno): implemented discipline creation

--- OrientadoAObjetos/CRUDAlunos/WinFormsDisciplinas/Form1.cs
+++ OrientadoAObjetos/CRUDAlunos/WinFormsDisciplinas/Form1.cs
@@ -15,14 +15,45 @@ namespace WinFormsDisciplinas
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
+            Disciplina novaDisciplina;
 
+            if (ValidaForm())
+            {
+                //construi o objeto
+                novaDisciplina = new Disciplina
+                {
+                    Id = contaDisciplina,
+                    Nome = txtNomeDisciplina.Text,
+                };
+
+                Disciplinas.Add(novaDisciplina);
+                contaDisciplina++;
+
+                InitLista();
+            }
+            else
+            {
+                MessageBox.Show("Preencha corretamente os dados e tente novamente.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void InitLista()
         {
-           
+           DisciplinaListBox.DataSource = null;        //limpa o interface 
+           DisciplinaListBox.DataSource = Disciplinas;  // associar lista ao DataSource da ListBox
+           DisciplinaListBox.DisplayMember = "Nome";
         }
+        private bool ValidaForm()
+        {
+            bool output = true;
 
-  
+            if (string.IsNullOrEmpty(txtNomeDisciplina.Text))
+            {
+                MessageBox.Show("Insira o nome da disciplina ", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                output = false;
+            }
+            return output;
+
+        }
     }
 }
