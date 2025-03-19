95d782c9596c12ec128261a9523731d45cfe47a2
vanessarcosta <vanessajreis38@gmail.com>
2025-03-19 21:54:37 +0000

    feat (Coourse): implemented inheritance

---
+++ OrientadoAObjetos/Coourse/Coourse/Program.cs
@@ -0,0 +1,16 @@
+﻿using Coourse.Entities;
+namespace Coourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //protected não consigo alterar aqui, apenas na subclasse
+            // account.Balance = 200.0;
+        }
+    }
+}
