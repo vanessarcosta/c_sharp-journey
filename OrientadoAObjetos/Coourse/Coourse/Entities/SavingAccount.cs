96cda6a813c7b95953019933b13b124393b10c8b
vanessarcosta <vanessajreis38@gmail.com>
2025-03-20 01:14:17 +0000

    feat (Coourse): demo upcasting and downcasting

---
+++ OrientadoAObjetos/Coourse/Coourse/Entities/SavingAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace Coourse.Entities
+{
+    class SavingAccount: Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccount()
+        {
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate)
+            :base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
--- OrientadoAObjetos/Coourse/Coourse/Program.cs
+++ OrientadoAObjetos/Coourse/Coourse/Program.cs
@@ -1,16 +1,53 @@
 ﻿using Coourse.Entities;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System;
 namespace Coourse
 {
     class Program
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            ////BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
 
-            Console.WriteLine(account.Balance);
+            ////Console.WriteLine(account.Balance);
 
             //protected não consigo alterar aqui, apenas na subclasse
             // account.Balance = 200.0;
+
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //upcasting
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingAccount(1004, "Anna", 0.0, 0.01);
+
+            //Downcasting
+            //implicitamente não pode    BusinessAccount acc4 = acc2;
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            // acc3 é SavingAccout subclasse que tipo # Business Account, não aparece o erro mas é incompativel
+            // Severity Code    Description Project File Line    Suppression State
+            //Error(active)  MSB3021 Unable to copy file "C:\projects\c_sharp-journey\OrientadoAObjetos\Coourse\Coourse\obj\Debug\net8.0\apphost.exe" to "bin\Debug\net8.0\Coourse.exe".The process cannot access the file 'bin\Debug\net8.0\Coourse.exe' because it is being used by another process.Coourse C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\Microsoft.Common.CurrentVersion.targets   5321
+            //            -- - ****BusinessAccount acc5 = (BusinessAccount)acc3;
+
+            //testar primeiro
+            if(acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan");
+            }
+
+            if (acc3 is SavingAccount)
+            {
+                SavingAccount acc5 = (SavingAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
