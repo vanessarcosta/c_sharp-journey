d49e0d1c0dd8b0b104b629320672e13b6bda1844
vanessarcosta <vanessajreis38@gmail.com>
2025-03-22 01:40:13 +0000

    feat (SalaryEmployee): create class employee

---
+++ OrientadoAObjetos/SalaryEmployee/SalaryEmployee/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace SalaryEmployee.Entities
+{
+    class Employee
+    {
+
+        public string Name {  get; private set; }
+        public int Hours { get; private set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() { }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public double Payment ()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+}
