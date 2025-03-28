46eeca43d2bf850db116f7ac033f6239554f8321
vanessarcosta <vanessajreis38@gmail.com>
2025-03-28 02:32:19 +0000

    fix (SimulacaoFinanciamentos): created class Fiador

---
+++ OrientadoAObjetos/SimulacaoFinanciamentos/Biblioteca/Fiador.cs
@@ -0,0 +1,17 @@
+﻿namespace Biblioteca
+{
+    public class Fiador
+    {
+        public string NomeFiador { get; set; }
+        public string MoradaFiador { get; set; }
+        public int TelefoneFiador { get; set; }
+        public int NifFiador { get; set; }
+        public double RendimentoFiador { get; set; }
+
+        public Fiador() { }
+        public override string ToString()
+        {
+            return  $"\nFiador: {NomeFiador},Morada: {MoradaFiador}, NIF: {NifFiador}, Rendimento: {RendimentoFiador:F2}";
+        }
+    }
+}
