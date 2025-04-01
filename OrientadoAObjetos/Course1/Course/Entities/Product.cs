8fe90735ca53559e9d0094d1a04cc47f75975a55
vanessarcosta <vanessajreis38@gmail.com>
2025-04-02 00:09:00 +0100

    feat (Course1): demo comparison

---
+++ OrientadoAObjetos/Course1/Course/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    class Product
+    ////class Product : IComparable<Product>     --implementação do IComparable *** colocar using System;
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        ///--implementação do IComparable***
+        ////public int CompareTo(Product other)
+        ////{
+        ////    //toupper converte para letra maiscula
+        ////    return Name.ToUpper().CompareTo(other.Name.ToUpper());   //se quiser comparar o preço tenho que alterar para Price.CompareTo(other.Price);
+        ////                                                             //  nesta forma a classe não está fechada para alteração ( viola o principio solid OPP)
+        ////                                                             //tenho que abrir a classe sempre que o critério de comparação mudar
+        ////}
+    }
+}
---
+++ OrientadoAObjetos/Course1/Course/Program.cs
@@ -0,0 +1,42 @@
+﻿using Course.Entities;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            ///list.Sort();    //ordenar a lista, so funciona se o tipo da lista (neste caso o Product) se ele implementa a interface IComparable
+
+            /*  list.Sort(CompareProducts);  */ ///não passo produtos como argumento simplesmente coloco o nome da função (referencia para função **DELEGATE**
+                                    //**referència simples de método como parâmetro
+
+            //PODEMOS tbm criar uma várivel  
+            //Comparison<Product> comp = CompareProducts;
+
+            //usando lambida   ***expressão lambida atribuída a uma variável tipo delegate
+            //Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+            //list.Sort(comp);
+
+            //colocando lambida diretamente no Sort   **expressão lambda inline
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        //**substituido por expressao lambida
+        ////static int CompareProducts(Product p1, Product p2)
+        ////{
+        ////    return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        ////}
+    }
+}
