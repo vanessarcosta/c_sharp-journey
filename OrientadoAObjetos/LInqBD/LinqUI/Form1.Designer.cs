81bd505bc562311ca8d7eb1a62b1764965fd7a3f
vanessarcosta <vanessajreis38@gmail.com>
2025-04-01 23:11:03 +0100

    feat (LinqBD): demo linq in Windws Forms

---
+++ OrientadoAObjetos/LInqBD/LinqUI/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LinqUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(37, 48);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(316, 291);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(437, 48);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(532, 291);
+            this.treeView1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 380);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 49;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(932, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 572);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "LINQ UI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ OrientadoAObjetos/LInqBD/LinqUI/Form1.cs
@@ -0,0 +1,106 @@
+﻿using LInqBD;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LinqUI
+{
+    public partial class Form1 : Form
+    {
+        DataClasses1DataContext dc = new DataClasses1DataContext();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ListView  *******
+            listView1.Columns.Add("ID");
+            listView1.Columns.Add("Titulo");
+            listView1.Columns.Add("Categoria");
+
+            //carregar os filmes
+
+            var lista = from Filme in dc.Filmes select Filme;
+
+            foreach (Filme filme in lista)
+            {
+                ListViewItem item;
+                item = listView1.Items.Add(filme.ID.ToString());
+                item.SubItems.Add(filme.Titulo);
+                item.SubItems.Add(filme.Categoria);
+            }
+
+            for (int i = 0; i <= 2; i++)
+            {
+                listView1.Columns[i].AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize); //organiza as colunas 
+            }
+
+            //TreeView *****
+
+            var outraLista = from Categoria in dc.Categorias select Categoria;
+
+            foreach (Categoria categoria in outraLista)
+            {
+                treeView1.Nodes.Add(categoria.Sigla);
+            }
+
+            //Segundo nível da árvore (Filmes)
+            var listaFilmes = from Filme in dc.Filmes
+                              orderby Filme.Titulo                                //distribuir os filmes pelo nos
+                              select Filme;
+
+            string catFilme;
+                                                                //dectaectar o no e depois colocar o filme
+           foreach (Filme filme in listaFilmes)
+           {
+                catFilme = filme.Categoria;
+
+                foreach (TreeNode node in treeView1.Nodes)
+                {
+                    if (node.Text == catFilme)
+                    {
+                        node.Nodes.Add(filme.Titulo);
+                    }
+                }
+           }
+
+            //GridView ******
+
+            dataGridView1.Columns.Add("colId", "ID");
+            dataGridView1.Columns.Add("colTitulo", "Titulo");
+            dataGridView1.Columns.Add("colCategoria", "Categoria");
+
+            var outraListaDeFilmes = from Filme in dc.Filmes select Filme;
+
+            int linha = 0;
+
+            DataGridViewCellStyle estilo = new DataGridViewCellStyle();
+            estilo.ForeColor = Color.Red;
+
+            foreach (Filme filme in outraListaDeFilmes)
+            {
+                DataGridViewRow registro = new DataGridViewRow();            //defini a linha do dataGrid
+                dataGridView1.Rows.Add(registro);                            //atribui linha nova dentro do dataGrid   
+
+                //carregar os dados a cada coluna
+                dataGridView1.Rows[linha].Cells[0].Value = filme.ID;
+                dataGridView1.Rows[linha].Cells[1].Value = filme.Titulo;
+                dataGridView1.Rows[linha].Cells[2].Value = filme.Categoria;
+
+                if ((string)dataGridView1.Rows[linha].Cells[2].Value == "FA")
+                {
+                    dataGridView1.Rows[linha].DefaultCellStyle = estilo;   ///aplicar o estilo 
+                }
+
+                linha++;
+
+            }
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+    }
+}
