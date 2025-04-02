b49f6cc6cb1ce8d33a8a9d5aefc7f368a4061881
vanessarcosta <vanessajreis38@gmail.com>
2025-04-02 01:02:51 +0100

    feat (Course2): demo delegate

---
+++ OrientadoAObjetos/Course2/Course2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Course2.Services;
+
+namespace Course2
+{
+    internal class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;  //delegate op é uma refencia para funçao Sum
+            //Sintaxe alternativa+++
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);+++
+            BinaryNumericOperation op1 = CalculationService.Max;
+
+            //BinaryNumericOperation op2 = CalculationService.Square;    // a assinatura da função (1) não casa com a função do delegate (2)
+            //no overload for 'Square' matches delegate 'BinaryNumericOperatio'
+
+            //double result = op.Invoke(a, b); ++++++
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+
+            result = op1(a, b);
+            Console.WriteLine(result);
+
+            result = CalculationService.Square(a);
+            Console.WriteLine(result);
+            
+            //sem delegate
+            ////double result = CalculationService.Sum(a, b);
+            ////Console.WriteLine(result);
+
+            ////result = CalculationService.Max(a, b);
+            ////Console.WriteLine(result);
+
+            //// result = CalculationService.Square(a);
+            ////Console.WriteLine(result);
+        }
+    }
+}
---
+++ OrientadoAObjetos/Course2/Course2/Services/CalculationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Course2.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
