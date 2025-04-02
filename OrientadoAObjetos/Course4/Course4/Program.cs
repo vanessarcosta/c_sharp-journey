69f593d355ab8a0ed603b4772045ea281b38ab91
vanessarcosta <vanessajreis38@gmail.com>
2025-04-02 15:46:39 +0100

    feat(Course4): demo introduction LINQ

---
+++ OrientadoAObjetos/Course4/Course4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace Course4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define the query expression
+            IEnumerable result = numbers        //  var result = numbers  
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            //List<int> result = numbers
+            //            .Where(x => x % 2 == 0)
+            //            .Select(x => x * 10).ToList();
+
+            //Execute the query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
