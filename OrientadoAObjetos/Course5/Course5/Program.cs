e42985717e0052c25b0114be9be2e0584750aab3
vanessarcosta <vanessajreis38@gmail.com>
2025-04-02 18:17:49 +0100

    feat(Course5): demo LINQ com notação similar a SQL

--- OrientadoAObjetos/Course5/Course5/Program.cs
+++ OrientadoAObjetos/Course5/Course5/Program.cs
@@ -35,27 +35,63 @@ namespace Course5
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 },
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+
+
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+            //sintaxe alternativa 
+            var r1 = 
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.00
+                select p;
             Print("TIER 1 AND PRICE ", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 = 
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
+
             Print("NAMES OF PRODUCTS FROM TOOLS", r2);
 
             //criar um objeto anonimo (que não está declarado em algumas das classes, é um obj especifico)  --new
             // p.Category.Name  o compilador dá erro pq tem dois Name, neste caso damos um apelido ao campo
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
             Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 =
+                (from p in r4
+                 select p).Skip(2).Take(4);            
             Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
 
-            var r6 = products.First();
+            //var r6 = products.First();
+            var r6 = (from p in products select p).First();
             Console.WriteLine("First test1: " + r6);
 
-            var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            //var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            var r7 =
+                (from p in products
+                where p.Price > 3000.0
+                select p).FirstOrDefault();
+
             Console.WriteLine("First or default test2: ", r7);
 
             var r8 = products.Where(p => p.Id == 3).SingleOrDefault();  ///tem que ter certeza se retorna um ou nenhum elemento, se + dá excessao
@@ -84,8 +120,11 @@ namespace Course5
             Console.WriteLine("Category 1 aggregate sum: " + r15);
 
             Console.WriteLine();
-            
-            var r16 = products.GroupBy(p => p.Category);
+
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 =
+                from p in products
+                group p by p.Category;
             foreach (IGrouping<Category, Product> group in r16)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
