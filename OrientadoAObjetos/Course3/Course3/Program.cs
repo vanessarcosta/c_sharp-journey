1035c57743b118779a7c0a1d764c4a1fdd36799e
vanessarcosta <vanessajreis38@gmail.com>
2025-04-02 01:38:02 +0100

    feat (Course3): demo delegate predicate

---
+++ OrientadoAObjetos/Course3/Course3/Program.cs
@@ -0,0 +1,32 @@
+﻿using Course3.Entities;
+namespace Course3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Fazer um programa que, a partir de uma lista de produtos, remova da
+            ////lista somente aqueles cujo preço mínimo seja 100.
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            ////list.RemoveAll(p => p.Price >= 100.00   // lambda
+
+            list.RemoveAll(ProductTest);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
