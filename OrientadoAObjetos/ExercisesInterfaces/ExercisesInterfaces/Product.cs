aace8b4dad33a20ad09e2c6fee977ac26a961871
vanessarcosta <vanessajreis38@gmail.com>
2025-05-16 17:29:17 +0100

    feat (ExerciseInterfaces): created class Product

---
+++ OrientadoAObjetos/ExercisesInterfaces/ExercisesInterfaces/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercisesInterfaces
+{
+    public abstract class Product
+    {
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+
+        public double Price { get; set; }
+
+        public double Tax { get; set; }
+
+        public override string ToString()
+        {
+            return $"Description:{Description}, Id: {Id}, Price: {Price}, Tax: {Tax}";
+        }
+    }
+}
