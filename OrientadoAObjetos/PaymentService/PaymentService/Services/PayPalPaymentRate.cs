4f7b872b66440ec070824fd5e34d4c426b9d4681
vanessarcosta <vanessajreis38@gmail.com>
2025-05-20 22:15:16 +0100

    feat(PaymentService): created class PayPalPaymentRate

---
+++ OrientadoAObjetos/PaymentService/PaymentService/Services/PayPalPaymentRate.cs
@@ -0,0 +1,15 @@
+﻿namespace PaymentService.Services
+{
+    class PayPalPaymentRate
+    {
+        public double Rate(double amount)
+        {
+            return amount * 0.02;
+        }
+
+        public double Fees(double amount)
+        {
+            return amount * 0.01;
+        }
+    }
+}
