1f8ed1309f8168bbfbc6fd35b8135cb3a6ef969f
vanessarcosta <vanessajreis38@gmail.com>
2025-05-21 16:33:34 +0100

    feat(PaymentService): created class GenerateInstallments

---
+++ OrientadoAObjetos/PaymentService/PaymentService/Services/GenerateInstallments.cs
@@ -0,0 +1,36 @@
+﻿using PaymentService.Entities;
+
+namespace PaymentService.Services
+{
+    class GenerateInstallments
+    {
+        List<Installment> listInstallments;
+        private IPaymentRate _paymentRate;
+
+        public GenerateInstallments(IPaymentRate paymentRate)
+        {
+            _paymentRate = paymentRate;
+        }
+
+        public void ProcessInstallments(Contract contract,double numberMonths)
+        {
+            double basicPayment = contract.TotalValue / numberMonths;
+
+            double installment = 0;
+
+            DateTime dueDate = contract.Date;
+
+            while (installment  > numberMonths)
+            {
+                double valueWithFess = _paymentRate.Fees(basicPayment);
+
+                double installmentValue = _paymentRate.Rate(valueWithFess);
+
+                dueDate = dueDate.AddMonths(1);
+                listInstallments.Add(new Installment(dueDate, installmentValue));
+
+                installment++;
+            }
+        }
+    }
+}
