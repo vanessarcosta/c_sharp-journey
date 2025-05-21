6ad5cae9ef880db7f458db9a29a7b74543396b5b
vanessarcosta <vanessajreis38@gmail.com>
2025-05-21 16:16:11 +0100

    feat(PaymentService): created constructor in class Installment

--- OrientadoAObjetos/PaymentService/PaymentService/Entities/Installment.cs
+++ OrientadoAObjetos/PaymentService/PaymentService/Entities/Installment.cs
@@ -5,5 +5,11 @@
         public DateTime DueDate { get; set; }
 
         public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
     }
 }
