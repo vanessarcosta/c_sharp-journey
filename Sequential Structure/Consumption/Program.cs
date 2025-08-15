b08bd7e912a9df72efef4503339821f928702547
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 17:17:22 +0100

    (Consumption): calculate the average consumption

---
+++ Sequential Structure/Consumption/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Consumption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance, fuel, averageConsumption;
+
+            Console.Write("Distancia percorrida: ");
+            distance = double.Parse(Console.ReadLine());
+            Console.Write("Combustivel gasto: ");
+            fuel = double.Parse(Console.ReadLine());
+            
+            averageConsumption = distance / fuel;
+            Console.WriteLine($"Consumo medio = {averageConsumption:F3}");
+            
+        }
+    }
+}
