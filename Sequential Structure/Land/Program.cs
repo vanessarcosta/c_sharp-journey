d466c67e7ed2ba8298876d459f5f6e98cf4df7cd
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 12:55:15 +0100

    (Land): area and value of a plot of land calculated

---
+++ Sequential Structure/Land/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Land
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width, length = 0.0, valueSquareMeter, area, price;
+
+            Console.Write("Digite a largura do terreno: ");
+            width = double.Parse(Console.ReadLine());
+            Console.Write("Digite o comprimento do terreno: ");
+            length = double.Parse(Console.ReadLine());
+            Console.Write("Digite o comprimento do terreno: ");
+            valueSquareMeter = double.Parse(Console.ReadLine());
+
+            area = width * length;
+            price = valueSquareMeter * area;
+
+            Console.WriteLine($"Area do terreno = {area:F2}");
+            Console.WriteLine($"Preco do terreno = {price:F2}");
+        }
+    }
+}
