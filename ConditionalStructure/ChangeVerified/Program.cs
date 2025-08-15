4f36d075b7edac698a59f4b3e92af84a3dabfc5d
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 22:24:24 +0100

    (ChangeVerified): check the change

---
+++ ConditionalStructure/ChangeVerified/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ChangeVerified
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price, money, change;
+            int quantity;
+
+            Console.Write("Preco unitario do produto: ");
+            price = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade comprada: ");
+            quantity = int.Parse(Console.ReadLine());
+            Console.Write("Dinheiro recebido: ");
+            money = double.Parse(Console.ReadLine());
+
+            change = money - price * quantity;
+
+            if (change >= 0)
+            {
+                Console.WriteLine($"TROCO = {change:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"DINHEIRO INSUFICIENTE. FALTAM {-1 * change} REAIS ");
+            }
+
+        }
+    }
+}
