7f10acc8923c4da05b6553e171767b8867a55835
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 16:32:26 +0100

    (Change): calculate the change

---
+++ Sequential Structure/Change/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Change
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price, moneyReceived, change;
+            int quantity;
+
+            Console.Write("Preco unitario do produto: ");
+            price = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade comprada: ");
+            quantity = int.Parse(Console.ReadLine());
+            Console.Write("Dinheiro recebido: ");
+            moneyReceived = double.Parse(Console.ReadLine());
+
+            change = moneyReceived - (price * quantity);
+            Console.WriteLine($"TROCO = {change:F2}");
+
+        }
+    }
+}
