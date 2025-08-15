845a10526decbfdf7aeaac25a55285b52944ed92
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 17:43:04 +0100

    (Duration): calculate duration of time

---
+++ Sequential Structure/Duration/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Duration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int duration, hours, remainder, minutes, seconds;
+
+            Console.Write("Digite a duração em segundos: ");
+            duration = int.Parse(Console.ReadLine());
+
+            hours = duration / 3600;
+            remainder = duration % 3600;
+
+            minutes = remainder / 60;
+            seconds = remainder % 60;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+}
