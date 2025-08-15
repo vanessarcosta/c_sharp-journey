9cac96b25582c34322f1bf2e1ef270fcc96795e3
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 15:53:49 +0100

    (Ages): calculate the average age

---
+++ Sequential Structure/Ages/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1, name2;
+            int age1, age2;
+            double average;
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            name1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            age1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            name2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            age2 = int.Parse(Console.ReadLine());
+
+            average = (double) (age1 + age2) / 2;
+            Console.WriteLine($"A idade média de {name1} e {name2} é de {average:F1} anos");
+
+        }
+    }
+}
