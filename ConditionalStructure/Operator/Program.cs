e640527d86b68c3d23cf7891fb3ba846c05716c0
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 22:13:59 +0100

    (Operator): calculate plan value

---
+++ ConditionalStructure/Operator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double plan = 50.0, valuePerMinute = 2.0, total;            
+            int minutes;
+
+            Console.Write("Digite a quantidade de minutos:");
+            minutes = int.Parse(Console.ReadLine());
+
+            if (minutes > 100)
+            {
+                plan = (minutes - 100) * valuePerMinute + plan;
+            }
+
+            Console.WriteLine($"Valor a pagar: R$ {plan}");
+        }
+    }
+}
