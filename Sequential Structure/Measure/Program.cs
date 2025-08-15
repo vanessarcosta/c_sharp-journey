f8f194b830d3f2f7b9f58beec0d1cf05f505815d
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 17:30:16 +0100

    (Measure): calculate area of geometric figures

---
+++ Sequential Structure/Measure/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Measure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double measureA, measureB, measureC, areaSquare, areaTriangle, areaTrapezoid;
+
+            Console.Write("Digite a medida A: ");
+            measureA = double.Parse(Console.ReadLine());
+            Console.Write("Digite a medida B: ");
+            measureB = double.Parse(Console.ReadLine());
+            Console.Write("Digite a medida C: ");
+            measureC = double.Parse(Console.ReadLine());
+
+            areaSquare = Math.Pow(measureA, 2);
+            areaTriangle = measureA * measureB / 2;
+            areaTrapezoid = (measureA + measureB) * measureC / 2;
+
+            Console.WriteLine($"AREA DO QUADRADO = {areaSquare:F4}");
+            Console.WriteLine($"AREA DO TRIANGULO = {areaTriangle:F4}");
+            Console.WriteLine($"AREA DO TRAPEZIO = {areaTrapezoid:F4}");
+
+        }
+    }
+}
