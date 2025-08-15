c41ba29c72829c979eeaaf7ca18ae389d4c44562
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 21:39:23 +0100

    (Grades): calculate grades

---
+++ ConditionalStructure/Grades/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade1, grade2, total;
+
+            Console.Write("Digite a primeira nota: ");
+            grade1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite a segunda nota: ");
+            grade2 = double.Parse(Console.ReadLine());
+
+            total = grade1 + grade2;
+
+            Console.WriteLine($"NOTA FINAL = {total:F1}");
+            if(total < 60)
+            {
+                Console.WriteLine("REPROVADO");
+            }
+        }
+    }
+}
