b18f1adc1255ea0fc9898bf56c04cab41fbda3b9
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 22:49:28 +0100

    (Temperature): convert temperature

---
+++ ConditionalStructure/Temperature/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahr;
+            char temperature;
+
+            Console.Write("Voce vai digitar a temperatura em qual escala (C/F)? ");
+            temperature = char.Parse(Console.ReadLine());
+
+            if (temperature == 'C')
+            {
+                Console.Write("Digite a temperatura em Celsius: ");
+                celsius = double.Parse(Console.ReadLine());
+                fahr = (celsius * 9) / 5 + 32;
+                Console.WriteLine($"Temperatura equivalente em Fahrenheit: {fahr:F2}");
+            }
+            else
+            {
+                Console.Write("Digite a temperatura em Fahrenheit:");
+                fahr = double.Parse(Console.ReadLine());
+                celsius = (fahr - 32) * 5 / 9;
+                Console.WriteLine($"Temperatura equivalente em Celsius: {celsius:F2}");
+            }
+
+        }
+    }
+}
