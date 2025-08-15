2195dda585b780c1bf3560e2e42a62bb59784bc5
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 15:43:16 +0100

    (Rectangle: calculate area, perimeter, diagonal of rectangle

---
+++ Sequential Structure/Rectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+
+namespace Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseRectangle, height, area, perimeter, diagonal;
+            Console.Write("Base do retangulo: ");
+            baseRectangle = double.Parse(Console.ReadLine());
+            Console.Write("Altura do retangulo: ");
+            height = double.Parse(Console.ReadLine());
+
+            area = baseRectangle * height;
+            perimeter = 2 * (baseRectangle + height);
+            diagonal = Math.Sqrt(Math.Pow(baseRectangle, 2.0) + Math.Pow(height, 2.0));
+
+            Console.WriteLine($"AREA = {area:F4}");
+            Console.WriteLine($"PERIMETRO = {perimeter:F4}");
+            Console.WriteLine($"DIAGONAL = {diagonal:F4}");
+        }
+    }
+}
