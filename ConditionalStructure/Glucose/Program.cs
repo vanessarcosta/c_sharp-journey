575c315c93420ec0b5406c59f5366c945d8862c8
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 22:31:13 +0100

    (Glucose): print glucose parameter

---
+++ ConditionalStructure/Glucose/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Glucose
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double glucose;
+
+            Console.Write("Digite a medida da glicose: ");
+            glucose = double.Parse(Console.ReadLine());
+
+            if (glucose <= 100.0)
+            {
+                Console.WriteLine("Classificacao: normal\n");
+            }
+            else if (glucose <= 140.0)
+            {
+                Console.WriteLine("Classificacao: elevado\n");
+            }
+            else
+            {
+                Console.WriteLine("Classificacao: diabetes\n");
+            }
+        }
+    }
+}
