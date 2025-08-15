498faa094adf9e01607f9b3b473930668a52a0a4
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 16:57:28 +0100

    (Circle): calculate the area of a circle

---
+++ Sequential Structure/Circle/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area; 
+
+            Console.Write("Digite o valor do raio do circulo: ");
+            radius = double.Parse(Console.ReadLine());
+
+            area = Math.Pow(radius, 2) * Math.PI;
+            Console.WriteLine($"AREA = {area:F3}");
+
+        }
+    }
+}
