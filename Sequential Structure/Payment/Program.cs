ecd78774db53e452531e95baad4a4a15cf69ed0f
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 17:08:50 +0100

    (Payment): calculate payment

---
+++ Sequential Structure/Payment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Payment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double valuePerHour, hoursWorked, payment;
+
+            Console.Write("Nome: ");
+            name = Console.ReadLine();
+            Console.Write("Valor por hora: ");
+            valuePerHour = double.Parse(Console.ReadLine());
+            Console.Write("Horas trabalhadas: ");
+            hoursWorked = double.Parse(Console.ReadLine());
+
+            payment = valuePerHour * hoursWorked;
+            Console.WriteLine($"O pagamento para {name} deve ser {payment:F2}");
+
+        }
+    }
+}
