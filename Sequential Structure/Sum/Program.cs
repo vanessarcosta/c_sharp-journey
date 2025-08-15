46543c7ab42c971af9820eb413e985d01cf4fc96
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 15:59:24 +0100

    (Sum): calculate the sum of two numbers

---
+++ Sequential Structure/Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, sum;
+
+            Console.Write("Digite o valor de X: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de Y: ");
+            y = int.Parse(Console.ReadLine());
+
+            sum = x + y;
+
+            Console.WriteLine($"SOMA = {sum}");
+        }
+    }
+}
