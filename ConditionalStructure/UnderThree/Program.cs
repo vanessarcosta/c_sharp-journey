9ce1c0977ca7831a4ad146cb4bf1d0edf84e4094
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-15 21:58:49 +0100

    (UnderThree): check the smallest of three numbers

---
+++ ConditionalStructure/UnderThree/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace UnderThree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3, smaller;
+
+            Console.Write("Primeiro valor: ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            number2 = int.Parse(Console.ReadLine());
+            Console.Write("Terceiro valor: ");
+            number3 = int.Parse(Console.ReadLine());
+
+            if(number1 < number2 && number1 < number3)
+            {
+                smaller = number1;
+            }
+            else if(number2 < number3)
+            { 
+                smaller = number2;
+            }
+            else
+            {
+                smaller = number3;
+            }
+
+            Console.WriteLine($"MENOR = {smaller}");
+
+        }
+    }
+}
