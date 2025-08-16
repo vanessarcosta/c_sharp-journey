e46c690043e09b0f1a241ee20f9867fa4d2e58d2
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 13:04:01 +0100

    (PlayingTime): calculate game duration

---
+++ ConditionalStructure/PlayingTime/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PlayingTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double startTime, endTime, duration;
+
+            Console.Write("Hora inicial: ");
+            startTime = double.Parse(Console.ReadLine());
+            Console.Write("Hora final: ");
+            endTime = double.Parse(Console.ReadLine());
+
+            if (endTime > startTime)
+            {
+                duration = endTime - startTime;                
+            }
+            else
+            {
+                duration = 24 - startTime + endTime;            
+            }
+            Console.WriteLine($"O JOGO DUROU {duration} HORA(S)");
+        }
+    }
+}
