a0acdb875a65d700229f6b9da108765be0bc7f76
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 20:56:02 +0100

    (InsideOutside): calculate the sum of odd numbers between a range

---
+++ RepetitiveStructures/InsideOutside/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace InsideOutside
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity, number, inside, outside;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            quantity = int.Parse(Console.ReadLine());
+
+            inside = 0;
+            outside = 0;
+
+            for (int i = 1; i <= quantity; i++)
+            {
+                Console.Write("Digite um numero: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number >= 10 && number <= 20)
+                {
+                    inside += 1;
+                }
+                else
+                {
+                    outside += 1;
+                }
+
+            }
+
+            Console.WriteLine($"{inside} DENTRO" );
+            Console.WriteLine($"{outside} FORA" );
+        }
+    }
+}
