7d7b61ec6c048dd159d516c3f9c525417bf90c6a
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 13:22:00 +0100

    (Coordinates): check which quadrant

---
+++ ConditionalStructure/Coordinates/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Coordinates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+
+            Console.Write("Valor de X: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Valor de Y: ");
+            y = double.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if(y == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else
+            {
+                Console.WriteLine("Eixo Y");
+            }
+        }
+    }
+}
