9579aa18188926a7526fbecb01c3cdc1cc06e146
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 18:09:10 +0100

    (FixedPassword): verify password

---
+++ RepetitiveStructures/FixedPassword/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FixedPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int password;
+
+            Console.Write("Digite a senha: ");
+            password = int.Parse(Console.ReadLine());
+
+            while (password != 2002)
+            {
+                Console.Write("Senha invalida! Tente novamente: ");
+                password = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido!");
+        }
+    }
+}
