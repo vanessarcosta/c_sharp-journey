ed32c6c18613da4edc98cfa1f219f75853d8be64
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 20:16:56 +0100

    (Fuel): search count

---
+++ RepetitiveStructures/Fuel/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Fuel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fuel, alcohol, gasoline, diesel;
+
+            alcohol = 0;
+            gasoline = 0;
+            diesel = 0;
+
+            Console.Write("Informe um codigo (1, 2, 3) ou 4 para parar:");
+            fuel = int.Parse(Console.ReadLine());
+
+            while (fuel != 4)
+            {
+                switch (fuel)
+                {
+                    case 1:
+                        alcohol += 1;
+                        break;
+                    case 2:
+                        gasoline += 1;
+                        break;
+                    case 3:
+                        diesel += 1;
+                        break;
+                }
+
+                Console.WriteLine("Informe um codigo (1, 2, 3) ou 4 para parar:");
+                fuel = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine($"Alcool: {alcohol}");
+            Console.WriteLine($"Gasolina {gasoline}");
+            Console.WriteLine($"Diesel: {diesel}" );
+
+        }
+    }
+}
