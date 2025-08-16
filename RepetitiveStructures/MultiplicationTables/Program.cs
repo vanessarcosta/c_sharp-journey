eecc32f0f69176a0052afccfc72408746b3926f7
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 20:32:38 +0100

    (MultiplicationTables): calculate the multiplication table

---
+++ RepetitiveStructures/MultiplicationTables/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MultiplicationTables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, total;
+
+            Console.Write("Deseja a tabuada para qual valor? ");
+            number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 11; i++)
+            {
+                total = number * i;
+
+                Console.WriteLine($"{number} x {i} = {total}");
+            }
+        }
+    }
+}
