2fd60eb79edb5f5bc80b50ae8d785edcf17278fd
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 18:40:28 +0100

    (GradeValidation): grade validation

---
+++ RepetitiveStructures/GradeValidation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace GradeValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade1, grade2, average;
+
+            Console.Write("Digite a primeira nota: ");
+            grade1 = double.Parse(Console.ReadLine());
+
+            while (grade1 < 0 || grade1 > 10)
+            {
+                Console.Write("Valor invalido! Tente novamente");
+                grade1 = double.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Digite a segunda nota: ");
+            grade2 = double.Parse(Console.ReadLine());
+
+            while (grade2 < 0 || grade2 > 10)
+            {
+                Console.Write("Valor invalido! Tente novamente");
+                grade2 = double.Parse(Console.ReadLine());
+            }
+
+            average = (grade1 + grade2) / 2;
+
+            Console.WriteLine($"MEDIA = {average:F2}");
+        }
+    }
+}
