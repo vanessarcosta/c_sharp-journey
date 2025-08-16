f8f1c6343dd2cf8d1d4376788f8533bc963fb0c2
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 20:42:37 +0100

    (OddSum): calculate the sum of odd numbers between a range

---
+++ RepetitiveStructures/OddSum/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace OddSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int oddSum, number1, number2, aux;
+
+            Console.WriteLine("Digite dois numeros:");
+            number1 = int.Parse(Console.ReadLine());
+            number2 = int.Parse(Console.ReadLine());
+
+            if (number1 > number2)
+            {
+                aux = number1;
+                number1 = number2;
+                number2 = aux;
+            }
+
+            oddSum = 0;
+
+            for (int i = number1 + 1; i < number2; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    oddSum = oddSum + i;
+                }
+
+            }
+
+            Console.WriteLine($"SOMA DOS IMPARES = {oddSum}" );
+        }
+    }
+}
