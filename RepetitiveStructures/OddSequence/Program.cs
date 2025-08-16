29163bc569fa4bd2c4cd25641cd92ac8d3af3026
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 20:48:50 +0100

    (OddSequence): calculate the sum of odd numbers between a range

---
+++ RepetitiveStructures/OddSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OddSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Digite o valor de X: ");
+            number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+        }
+    }
+}
