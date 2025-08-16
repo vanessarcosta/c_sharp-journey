ae4caa00397ab966db260b9bb39940eb286a83e4
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 13:58:29 +0100

    (AverageAges): calculate average age

---
+++ RepetitiveStructures/AverageAges/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AverageAges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age, total = 0, counter = 0;
+            double average = 0.0;
+
+            Console.WriteLine("Digite as idades");
+            age = int.Parse(Console.ReadLine());
+
+            if (age < 0)
+            {
+                Console.WriteLine("IMPOSSIVEL CALCULAR");
+            }
+            else
+            {
+
+                while (age > 0)
+                {
+                    total += age;
+                    age = int.Parse(Console.ReadLine());
+                    counter++;
+                }
+
+                average = (double)total / counter;
+                Console.WriteLine($"MEDIA = {average:F2}");
+            }
+
+        }
+    }
+}
