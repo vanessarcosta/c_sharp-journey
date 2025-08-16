a15e7705a9e401539ec9b18864aee90280ea9a08
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 20:24:45 +0100

    (ConsecutivePairs): print the sum of 5 consecutive pairs

---
+++ RepetitiveStructures/ConsecutivePairs/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsecutivePairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum;
+
+            Console.Write("Digite um numero inteiro: ");
+            number = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                if (number % 2 != 0)
+                {
+                    number = number + 1;
+                }
+
+                sum = 5 * number + 20;
+
+                Console.WriteLine($"SOMA = {sum}");
+
+                Console.Write("Digite um numero inteiro: ");
+                number = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
