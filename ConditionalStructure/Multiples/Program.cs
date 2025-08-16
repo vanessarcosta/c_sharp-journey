688e38448345e6bfc38d3a79589cb3c165cc05df
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 11:37:22 +0100

    (Multiples): check if the numbers are multiples

---
+++ ConditionalStructure/Multiples/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("Digite dois numeros inteiros:");
+            number1 = int.Parse(Console.ReadLine());
+            number2 = int.Parse(Console.ReadLine());
+
+            if (number1 % number2 == 0 || number2 % number1 == 0)
+            {
+                Console.WriteLine("Sao multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao multiplos");
+            }
+
+        }
+    }
+}
