cb6a68284c0da72a0866d1b371d33d6a6ffc249c
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 21:26:48 +0100

    (Factorial): calculate the factorial

---
+++ RepetitiveStructures/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, factorial;
+
+            Console.Write("Digite o valor de N: ");
+            N = int.Parse(Console.ReadLine());
+
+            factorial = 1;
+
+            for (int i = 1; i <= N; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine($"FATORIAL = {factorial}" );
+
+        }
+    }
+}
