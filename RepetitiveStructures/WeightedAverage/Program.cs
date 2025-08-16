8b2721bc942855ae2b09d3159c8c53b379b867fa
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 21:13:18 +0100

    (WeightedAverage): calculate the weighted average

---
+++ RepetitiveStructures/WeightedAverage/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace WeightedAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1, number2, number3, average;
+            int quantity;
+
+            Console.Write("Quantos casos voce vai digitar? ");
+            quantity = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= quantity; i++)
+            {
+                Console.WriteLine("Digite tres numeros:");
+                number1 = double.Parse(Console.ReadLine());
+                number2 = double.Parse(Console.ReadLine());
+                number3 = double.Parse(Console.ReadLine());
+
+                average = ((number1 * 2.0) + (number2 * 3.0) + (number3 * 5.0)) / 10.0;
+                Console.WriteLine($"MEDIA = {average:F1}");
+            }
+        }
+    }
+}
