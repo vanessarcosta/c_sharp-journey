35dbae7804efb8fa52df369fd47663c80208f7ea
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-16 13:37:28 +0100

    (Increasing): check if the order is ascending

---
+++ RepetitiveStructures/Increasing/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Increasing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("Digite dois numeros:");
+            number1 = int.Parse(Console.ReadLine());
+            number2 = int.Parse(Console.ReadLine());
+
+            while (number1 != number2)
+            {
+                if (number1 > number2)
+                {
+                    Console.WriteLine("Decrescente!");
+                }
+                else
+                {
+                    Console.WriteLine("Crescente!");
+                }
+                Console.WriteLine("Digite dois numeros:");
+                number1 = int.Parse(Console.ReadLine());
+                number2 = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
