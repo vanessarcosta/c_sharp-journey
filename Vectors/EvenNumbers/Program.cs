eee10e3a7c84435cc5de26af352257d971cefd66
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-19 21:17:44 +0100

    (EvenNumbers): print the number of even numbers

---
+++ Vectors/EvenNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace EvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, quantity = 0;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Digite um numero: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nNUMEROS PARES:");
+
+            for (int i = 0; i < N; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.Write($"{numbers[i]} ");
+                    quantity++;
+                }
+            }
+
+            Console.WriteLine($"\nQUANTIDADE DE PARES = {quantity}");
+        }
+    }
+}
