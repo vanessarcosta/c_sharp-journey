45abc84b13bcf656c21607c9d9c686fcb00edb41
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-19 22:02:12 +0100

    (Older): print the oldest person

---
+++ Vectors/Older/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Older
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, position, older, i = 0;
+
+            Console.WriteLine("Quantas pessoas voce vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+
+            string[] names = new string[N];
+            int[] ages = new int[N];
+
+            for (i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Dados da {i + 1}a pessoa:" );
+                Console.Write("Nome: ");
+                names[i] = Console.ReadLine();
+                Console.Write("Idade: ");
+                ages[i] = int.Parse(Console.ReadLine());
+            }
+
+            older = 0;
+            position = 0;
+
+            for (i = 0; i < N; i++)
+            {
+                if (older < ages[i])
+                {
+                    older = ages[i];
+                    position = i;
+                }
+            }
+
+            for (i = 0; i < N; i++)
+            {
+                if (position == i)
+                {
+                    Console.WriteLine($"PESSOA MAIS VELHA: {names[i]}" );
+                }
+            }
+        }
+    }
+}
