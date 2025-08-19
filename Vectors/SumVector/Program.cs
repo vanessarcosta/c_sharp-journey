93421957e02640ef37f481aab2172c7f215e53bc
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-19 21:34:23 +0100

    (SumVector): sum of two vectors

---
+++ Vectors/SumVector/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace SumVector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            Console.Write("Quantos valores vai ter cada vetorr? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[] vectorA = new int[N];
+            int[] vectorB = new int[N];
+            int[] vectorC = new int[N];
+
+            Console.WriteLine("Digite os valores do vetor A:");
+
+            for (int i = 0; i < N; i++)
+            {
+                vectorA[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Digite os valores do vetor B:");
+
+            for (int i = 0; i < N; i++)
+            {
+                vectorB[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                vectorC[i] = vectorA[i] + vectorB[i];
+            }
+
+            Console.WriteLine("VETOR RESULTANTE:");
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(vectorC[i]);
+            }
+        }
+    }
+}
