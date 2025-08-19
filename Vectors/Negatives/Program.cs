21697ef6423290154dab27f59574b5e482d4effb
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-19 20:16:32 +0100

    (Negatives): print negative numbers

---
+++ Vectors/Negatives/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Negatives
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            
+            Console.Write("Quantos numeros voce vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[] numbers =  new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Digite um numero: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("NUMEROS NEGATIVOS: ");
+            for (int i = 0; i < N; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    Console.WriteLine($"{numbers[i]} ");
+                }
+            }
+
+
+        }
+    }
+}
