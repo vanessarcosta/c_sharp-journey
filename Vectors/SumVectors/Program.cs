3d767d72ed7005656a03d9a8f2e0adbe57e72706
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-19 20:34:46 +0100

    (SumVectors): calculate sum and average of a vector

---
+++ Vectors/SumVectors/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SumVectors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double sum = 0, average;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+
+            double[] numbers = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Digite um numero: ");
+                numbers[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.Write("\nVALORES = ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"{numbers[i]:F2} ");
+
+            }
+
+            for(int i = 0;i < N;i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine($"\nSOMA = {sum:F2}");
+
+            average = sum / N;
+            Console.WriteLine($"MEDIA = {average:F2}");
+
+        }
+    }
+}
