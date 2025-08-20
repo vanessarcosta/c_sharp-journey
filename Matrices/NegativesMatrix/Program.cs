5de3279445869487ae00e105108887eaca218df4
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-20 23:00:30 +0100

    (NegativesMatrix): print the negative elements

---
+++ Matrices/NegativesMatrix/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace NegativesMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int M, N, quantity = 0;
+
+            Console.Write("Qual a quantidade de linhas da matriz? ");
+            M = int.Parse(Console.ReadLine());
+            Console.Write("Qual a quantidade de colunas da matriz? ");
+            N = int.Parse(Console.ReadLine());
+
+            int [,] matrix = new int[M,N] ;
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($"Elemento [{i},{j}]: ");
+                    matrix[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("VALORES NEGATIVOS:");
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (matrix[i,j] < 0)
+                    {
+                        Console.WriteLine(matrix[i,j]);
+                    }
+                }
+            }
+        }
+    }
+}
