afb07cde317466d0d4d3c46032d6a0a8ccd6db60
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-20 23:28:15 +0100

    (AboveDiagonal): print the sum of the elements above the main diagonal

---
+++ Matrices/AboveDiagonal/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace AboveDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, sum = 0;
+
+            Console.WriteLine("Qual a ordem da matriz? ");
+            N = int.Parse(Console.ReadLine());
+
+            int [,] matrix = new int [N,N] ;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($"Elemento [{i},{j}]: ");
+                    matrix[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i < j)
+                    {
+                        sum += matrix[i,j];
+                    }
+                }
+            }
+
+            Console.WriteLine($"SOMA DOS ELEMENTOS ACIMA DA DIAGONAL PRINCIPAL = {sum}");
+        }
+    }
+}
