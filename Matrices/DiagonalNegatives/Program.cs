2282ed6baa328d1f6a970a0fd276e6867e0b544d
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-20 22:39:17 +0100

    (DiagonalNegatives): print the main diagonal and the negatives

---
+++ Matrices/DiagonalNegatives/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace DiagonalNegatives
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, quantity = 0;
+
+            Console.Write("Qual a ordem da matriz? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($"Elemento [{i},{j}]: ");
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write($"{matrix[i,j]} ");
+                    }
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (matrix[i,j] < 0)
+                    {
+                        quantity++;
+                    }
+                }
+            }
+
+            Console.Write($"\nQUANTIDADE DE NEGATIVOS = {quantity}");
+        }
+    }
+}
