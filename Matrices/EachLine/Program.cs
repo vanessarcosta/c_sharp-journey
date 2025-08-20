1dc80664a74a86bc4456b1c051b9c7479c7dbaac
vanessarcosta <102038994+vanessarcosta@users.noreply.github.com>
2025-08-20 23:08:54 +0100

    (EachLine): print the largest element of each line

---
+++ Matrices/EachLine/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EachLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, bigger = 0;
+
+            Console.Write("Qual a ordem da matriz? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($"Elemento [{i},{j}]: ");
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("MAIOR ELEMENTO DE CADA LINHA:");
+
+            for (int i = 0; i < N; i++)
+            {
+                bigger = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    if (bigger < matrix[i,j])
+                    {
+                        bigger = matrix[i,j];
+                    }
+                }
+                Console.WriteLine(bigger);
+            }
+        }
+    }
+}
